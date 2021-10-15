4319f6f81bb00136d269de2a83fbc39c530f57bd
Colfax Selby <colfax.selby@gmail.com>
2021-10-15 12:38:54 -0400

    Prep for EB deployment, add buildspec

--- libs/ddbClient.ts
+++ libs/ddbClient.ts
@@ -1,9 +1,13 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { fromIni } from "@aws-sdk/credential-provider-ini";
 
-const ddbClient = new DynamoDBClient({
+const environment = process.env.NODE_ENV as string;
+
+const DYNAMO_DB_OPTIONS = environment == "dev" ? {
   region: "us-east-2",
   credentials: fromIni({profile: 'colfax-gitpoap'})
-});
+} : {};
+
+const ddbClient =  new DynamoDBClient(DYNAMO_DB_OPTIONS);
 
 export { ddbClient };
