4d6e6f52eb039d4070cf91aa755a109927a4dccc
Colfax Selby <colfax.selby@gmail.com>
2021-10-15 13:04:18 -0400

    Update aws sdk config for prod

--- libs/ddbClient.ts
+++ libs/ddbClient.ts
@@ -6,7 +6,9 @@ const environment = process.env.NODE_ENV as string;
 const DYNAMO_DB_OPTIONS = environment == "dev" ? {
   region: "us-east-2",
   credentials: fromIni({profile: 'colfax-gitpoap'})
-} : {};
+} : {
+  region: "us-east-2"
+};
 
 const ddbClient =  new DynamoDBClient(DYNAMO_DB_OPTIONS);
 
