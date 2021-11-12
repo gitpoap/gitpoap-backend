ef5803183918b85ba3b26b33a9a88ef3aea7e4b3
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-11-12 16:09:57 -0500

    add /suggest

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 import express from 'express';
 import subscribeRouter from './routes/subscribe';
+import { suggestRouter } from './routes/suggest';
 import jwtRouter from './routes/jwt';
 import cors from 'cors';
 
@@ -14,8 +15,10 @@ app.get('/', (req, res) => {
     res.send('Hello world.  -GitPOAP');
 })
 
+/* Endpoints */
 app.use('/jwt', jwtRouter);
 app.use('/subscribe', subscribeRouter);
+app.use('/suggest', suggestRouter);
 
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
