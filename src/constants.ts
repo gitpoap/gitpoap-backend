8284a15669d77e28b0752c6e34e52054dfb9e141
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-12-14 14:19:53 -0500

    add gh client id and secret

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,7 @@
 export const PORT = 3001;
 export const JWT_EXP_TIME = 60 * 10;
+
+/* Constants used for connection to GitHub */
+export const GH_APP_CLIENT_ID = "2c27505d957d66f74122";
+export const GH_APP_CLIENT_SECRET = "62f2f29b8f77bc529c3010731364c5aab94a9ef9";
+export const GH_APP_REDIRECT_URL = "http://localhost:3000/login";
