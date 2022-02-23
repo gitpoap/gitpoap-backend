90cf0cb8d22c4081077809e133e323b3bd8f4c1d
Anthony Burzillo <burz@gitpoap.io>
2022-02-23 14:40:51 -0500

    Use POST /projects not POST /flows/add-project

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import subscribeRouter from './routes/subscribe';
 import { suggestRouter } from './routes/suggest';
 import jwtRouter from './routes/jwt';
 import { githubRouter } from './routes/github';
-import { flowsRouter } from './routes/flows';
+import { projectsRouter } from './routes/projects';
 import { CONTACTS_TABLE_NAME } from './dynamo';
 import { PORT } from './constants';
 import { getSchema } from './graphql/schema';
@@ -28,9 +28,11 @@ const main = async () => {
   app.use('/jwt', jwtRouter);
   app.use('/subscribe', subscribeRouter);
   app.use('/suggest', suggestRouter);
-  app.use('/flows', flowsRouter);
   app.use('/github', githubRouter);
   app.use('/graphql', graphqlHTTP({ schema: await getSchema, context, graphiql: true }));
+
+  /* API endpoints for the frontend */
+  app.use('/projects', projectsRouter);
 
   app.listen(PORT, () => {
     console.log(`The application is listening on port ${PORT}\n`);
