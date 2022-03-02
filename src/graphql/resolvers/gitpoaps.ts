083de524e28b7e40bb7e73b74b786b4c726e2f72
Anthony Burzillo <burz@gitpoap.io>
2022-03-02 17:34:25 -0500

    Set up splitting and fetching of {,Git}POAPs

--- src/graphql/resolvers/gitpoaps.ts
+++ src/graphql/resolvers/gitpoaps.ts
@@ -1,7 +1,27 @@
-import { Arg, Ctx, Resolver, Query } from 'type-graphql';
-import { GitPOAP } from '@generated/type-graphql';
+import { Arg, Ctx, Field, ObjectType, Resolver, Query } from 'type-graphql';
+import fetch from 'cross-fetch';
+import { ClaimStatus, GitPOAP } from '@generated/type-graphql';
 import { getLastWeekStartDatetime } from './util';
 import { Context } from '../../context';
+import { POAPToken } from '../types/poap';
+
+@ObjectType()
+class FullGitPOAPData {
+  @Field(() => GitPOAP)
+  gitPOAP: GitPOAP;
+
+  @Field(() => POAPToken)
+  POAP: POAPToken;
+}
+
+@ObjectType()
+class UserPOAPs {
+  @Field(() => [FullGitPOAPData])
+  gitPOAPs: FullGitPOAPData[];
+
+  @Field(() => [POAPToken])
+  POAPs: POAPToken[];
+}
 
 @Resolver(of => GitPOAP)
 export class CustomGitPOAPResolver {
@@ -22,5 +42,60 @@ export class CustomGitPOAPResolver {
       },
     });
     return result._count.id;
+  }
+
+  @Query(returns => UserPOAPs, { nullable: true })
+  async userPOAPs(
+    @Ctx() { prisma }: Context,
+    @Arg('address') address: string,
+  ): Promise<UserPOAPs | null> {
+    // TODO: accept ENS + handle errors gracefully
+    try {
+      const poapResponse = await fetch(`${process.env.POAP_URL}/actions/scan/${address}`);
+
+      if (poapResponse.status >= 400) {
+        console.log(await poapResponse.text());
+
+        return null;
+      }
+
+      const poaps = await poapResponse.json();
+
+      console.log(poaps);
+
+      const gitPOAPs = await prisma.claim.findMany({
+        where: {
+          address: address,
+          status: ClaimStatus.CLAIMED,
+        },
+      });
+
+      let foundGitPOAPIds = {};
+      for (let gitPOAP of gitPOAPs) {
+        foundGitPOAPIds[gitPOAP.poapTokenId] = gitPOAP;
+      }
+
+      let gitPOAPsOnly = [];
+      let poapsOnly = [];
+      for (let poap of poaps) {
+        if (foundGitPOAPIds.hasOwnProperty(poap.tokenId)) {
+          gitPOAPsOnly.push({
+            gitPOAP: foundGitPOAPIds[poap.tokenId],
+            POAP: poap,
+          });
+        } else {
+          poapsOnly.push(poap);
+        }
+      }
+
+      return {
+        gitPOAPs: gitPOAPsOnly,
+        POAPs: poapsOnly,
+      };
+    } catch (err) {
+      console.log(err);
+
+      return null;
+    }
   }
 }
---
+++ src/graphql/types/poap.ts
@@ -0,0 +1,61 @@
+import { Field, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export class POAPEvent {
+  @Field()
+  id: Number;
+
+  @Field()
+  fancy_id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  event_url: string;
+
+  @Field()
+  image_url: string;
+
+  @Field()
+  country: string;
+
+  @Field()
+  city: string;
+
+  @Field()
+  description: string;
+
+  @Field()
+  year: Number;
+
+  @Field()
+  start_date: string;
+
+  @Field()
+  end_date: string;
+
+  @Field()
+  expiry_date: string;
+
+  @Field()
+  supply: Number;
+}
+
+@ObjectType()
+export class POAPToken {
+  @Field(() => POAPEvent)
+  event: POAPEvent;
+
+  @Field()
+  tokenId: string;
+
+  @Field()
+  owner: string;
+
+  @Field()
+  chain: string;
+
+  @Field()
+  created: string;
+}
