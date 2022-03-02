5c8958211af692c3119fed24ba5d25bafcf48377
Anthony Burzillo <burz@gitpoap.io>
2022-03-02 00:04:50 -0500

    Get basic resolver working

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,8 +1,23 @@
-import { buildSchema } from 'type-graphql';
-import { resolvers } from '@generated/type-graphql';
+import { Arg, buildSchema, Ctx, Resolver, Query } from 'type-graphql';
+import { resolvers, User } from '@generated/type-graphql';
+
+console.log(resolvers)
+
+@Resolver(of => User)
+class CustomResolver {
+  @Query(returns => [User])
+  async topContributors(
+    @Ctx() { prisma }: Context,
+    @Arg("count", { defaultValue: 10 }) count: number,
+  ): Promise<User[]> {
+    let users = await prisma.users.findMany();
+    return users;
+  }
+};
+console.log([...resolvers, CustomResolver])
 
 export const getSchema = buildSchema({
-  resolvers,
+  resolvers: [...resolvers, CustomResolver],
   emitSchemaFile: true,
   validate: false,
 });
