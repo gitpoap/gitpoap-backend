735fcf75638282cfc499d8581eea2132b8bf8307
Anthony Burzillo <burz@gitpoap.io>
2022-03-03 00:15:38 -0500

    Add fake POAP API endpoint for scanning poaps

--- .dockerfiles/fake-poap-api/index.js
+++ .dockerfiles/fake-poap-api/index.js
@@ -59,6 +59,57 @@ app.post('/events', (req, res) => {
   );
 });
 
+app.get('/actions/scan/:address', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  res.end(
+    JSON.stringify([
+      {
+        event: {
+          id: 1,
+          fancy_id: 'welcome-to-the-thunderdome',
+          name: 'Welcome to the Thunderdome',
+          event_url: 'https://thunderdome.xyz',
+          image_url: 'https://avatars.githubusercontent.com/u/1555326?v=4',
+          country: '',
+          city: '',
+          description: 'Wow, you survived.\nGreat',
+          year: 2022,
+          start_date: '2022-02-02',
+          end_date: '2026-02-02',
+          expiry_date: '2092-12-11',
+          supply: '10',
+        },
+        tokenId: 'thunderdome',
+        owner: req.params.address,
+        chain: 'xdai',
+        created: '2022-02-02',
+      },
+      {
+        event: {
+          id: 2,
+          fancy_id: 'ethdenver',
+          name: 'ethdenver',
+          event_url: 'https://eth.denver',
+          image_url: 'https://avatars.githubusercontent.com/u/1455326?v=4',
+          country: '',
+          city: '',
+          description: '#eth #denver #2022',
+          year: 2022,
+          start_date: '2022-02-11',
+          end_date: '2022-02-23',
+          expiry_date: '2022-03-28',
+          supply: '20',
+        },
+        tokenId: 'ethdenver',
+        owner: req.params.address,
+        chain: 'xdai',
+        created: '2022-02-01',
+      },
+    ]),
+  );
+});
+
 app.listen(port, () => {
   console.log(`Fake POAP API listening on port ${port}`);
 });
