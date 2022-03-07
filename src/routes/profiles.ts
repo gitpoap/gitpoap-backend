1166cc0bd1b3d6a71d89a60501efaba487518d5f
Anthony Burzillo <burz@gitpoap.io>
2022-03-07 15:27:34 -0500

    Check signature for the data via ethers

--- src/routes/profiles.ts
+++ src/routes/profiles.ts
@@ -1,6 +1,7 @@
 import { UpdateProfileSchema } from '../schemas/profiles';
 import { Router } from 'express';
 import { context } from '../context';
+import { utils } from 'ethers';
 
 export const profilesRouter = Router();
 
@@ -19,6 +20,16 @@ profilesRouter.post('/', async function (req, res) {
     if (resolvedAddress === null) {
       return res.status(400).send({ msg: `${req.body.address} is not a valid address` });
     }
+  }
+
+  // Reconstruct the message as prepared by ethers on the frontend
+  // See: https://docs.ethers.io/v5/api/signer/#Signer-signMessage
+  const message = utils.hashMessage(JSON.stringify(req.body.data));
+
+  // Validate the signature for the updates
+  const recoveredAddress = utils.verifyMessage(message, req.body.signature);
+  if (recoveredAddress !== resolvedAddress) {
+    return res.status(401).send({ msg: 'The signature is not valid for this address and data' });
   }
 
   try {
