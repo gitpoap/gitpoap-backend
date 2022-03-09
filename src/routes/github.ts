e63e0284536c74204cdb4de817718ac90e9cd2de
Anthony Burzillo <burz@gitpoap.io>
2022-03-09 13:23:32 -0500

    Setup auth skeleton

--- src/routes/github.ts
+++ src/routes/github.ts
@@ -1,42 +1,66 @@
 import { Router } from 'express';
-import { GH_APP_CLIENT_ID, GH_APP_CLIENT_SECRET, GH_APP_REDIRECT_URL } from '../constants';
+import {
+  GH_APP_CLIENT_ID,
+  GH_APP_CLIENT_SECRET,
+  GH_APP_REDIRECT_URL,
+  JWT_EXP_TIME,
+} from '../constants';
 import fetch from 'cross-fetch';
+import { sign } from 'jsonwebtoken';
+import { v4 } from 'uuid';
 
 export const githubRouter = Router();
 
+async function retrieveGithubToken(code: string) {
+  // Request to GitHub -> exchange code (from request body) for a GitHub access token
+  const body = {
+    client_id: GH_APP_CLIENT_ID,
+    client_secret: GH_APP_CLIENT_SECRET,
+    code,
+    redirect_uri: GH_APP_REDIRECT_URL,
+  };
+
+  const tokenRes = await fetch(`https://github.com/login/oauth/access_token`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+
+  const tokenJson = await tokenRes.json();
+  console.log('Token JSON: ', tokenJson);
+  if (tokenJson?.error) {
+    /* don't use JSON.stringify long term here */
+    throw JSON.stringify(tokenJson);
+  }
+
+  return tokenJson.access_token;
+}
+
+function generateAccessToken(githubToken: string) {
+  return sign({ githubToken }, process.env.JWT_SECRET as string, {
+    expiresIn: JWT_EXP_TIME,
+  });
+}
+
+function generateRefreshToken() {
+  // Generate a random key
+  const secret = v4();
+
+  return sign({ secret }, process.env.JWT_SECRET as string);
+}
+
 githubRouter.post('/', async function (req, res) {
   const { code } = req.body;
+  console.log(req.body);
 
   console.log('GitHub code: ', code);
-  let access_token = null;
+  let githubToken = null;
 
-  // Request to GitHub -> exchange code (from request body) for a GitHub access token
   try {
-    const body = {
-      client_id: GH_APP_CLIENT_ID,
-      client_secret: GH_APP_CLIENT_SECRET,
-      code,
-      redirect_uri: GH_APP_REDIRECT_URL,
-    };
-
-    const tokenRes = await fetch(`https://github.com/login/oauth/access_token`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-
-    const tokenJson = await tokenRes.json();
-    console.log('Token JSON: ', tokenJson);
-    if (tokenJson?.error) {
-      /* don't use JSON.stringify long term here */
-      throw JSON.stringify(tokenJson);
-    }
-
-    access_token = tokenJson.access_token;
-    console.log('Access token: ', tokenJson.access_token);
+    githubToken = await retrieveGithubToken(code);
   } catch (error) {
     console.error('An error has occurred', error);
     return res.status(400).send({
@@ -45,5 +69,8 @@ githubRouter.post('/', async function (req, res) {
     });
   }
 
-  return res.status(200).json({ token: access_token });
+  return res.status(200).json({
+    accessToken: generateAccessToken(githubToken),
+    refreshToken: generateRefreshToken(),
+  });
 });
