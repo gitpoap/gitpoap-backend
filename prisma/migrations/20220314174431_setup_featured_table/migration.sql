b67849f7c8cb1c80eb6e0c9f37c889159af0e536
Anthony Burzillo <burz@gitpoap.io>
2022-03-14 13:46:50 -0400

    Setup featured backend actions

--- prisma/migrations/20220314174431_setup_featured_table/migration.sql
+++ prisma/migrations/20220314174431_setup_featured_table/migration.sql
@@ -1,11 +1,14 @@
 -- CreateTable
 CREATE TABLE "FeaturedPOAP" (
     "id" SERIAL NOT NULL,
-    "poapTokenId" INTEGER NOT NULL,
+    "poapTokenId" VARCHAR(255) NOT NULL,
     "profileId" INTEGER NOT NULL,
 
     CONSTRAINT "FeaturedPOAP_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FeaturedPOAP_poapTokenId_profileId_key" ON "FeaturedPOAP"("poapTokenId", "profileId");
 
 -- AddForeignKey
 ALTER TABLE "FeaturedPOAP" ADD CONSTRAINT "FeaturedPOAP_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/routes/featured.ts
+++ src/routes/featured.ts
@@ -1,14 +1,60 @@
 import { Router } from 'express';
 import { AddFeaturedSchema, RemoveFeaturedSchema } from '../schemas/featured';
+import { context } from '../context';
+import { resolveENS } from '../util';
+import { utils } from 'ethers';
 
 export const featuredRouter = Router();
 
-featuredRouter.post('/', async function (req, res) {
+featuredRouter.put('/', async function (req, res) {
   const schemaResult = AddFeaturedSchema.safeParse(req.body);
 
   if (!schemaResult.success) {
     return res.status(400).send({ issues: schemaResult.error.issues });
   }
+
+  console.log(
+    `Received a request from ${req.body.address} to feature POAP ID: ${req.body.poapTokenId}`,
+  );
+
+  // Resolve ENS if provided
+  const resolvedAddress = await resolveENS(context.provider, req.body.address);
+  if (resolvedAddress === null) {
+    return res.status(400).send({ msg: `${req.body.address} is not a valid address` });
+  }
+
+  const recoveredAddress = utils.verifyMessage(
+    JSON.stringify({ action: 'add', id: req.body.poapTokenId }),
+    req.body.signature,
+  );
+  if (recoveredAddress !== resolvedAddress) {
+    return res.status(401).send({ msg: 'The signature is not valid for this address and data' });
+  }
+
+  const profile = await context.prisma.profile.findUnique({
+    where: {
+      address: resolvedAddress.toLowerCase(),
+    },
+  });
+  if (profile === null) {
+    return res.status(404).send({ msg: `There is no profile for the address ${req.body.address}` });
+  }
+
+  await context.prisma.featuredPOAP.upsert({
+    where: {
+      poapTokenId_profileId: {
+        poapTokenId: req.body.poapTokenId,
+        profileId: profile.id,
+      },
+    },
+    update: {},
+    create: {
+      poapTokenId: req.body.poapTokenId,
+      profileId: profile.id,
+    },
+  });
+
+  return res.status(200).send('ADDED');
 });
 
 featuredRouter.delete('/:id', async function (req, res) {
@@ -17,4 +63,42 @@ featuredRouter.delete('/:id', async function (req, res) {
   if (!schemaResult.success) {
     return res.status(400).send({ issues: schemaResult.error.issues });
   }
+
+  console.log(
+    `Received a request from ${req.body.address} to stop featuring POAP ID: ${req.params.id}`,
+  );
+
+  // Resolve ENS if provided
+  const resolvedAddress = await resolveENS(context.provider, req.body.address);
+  if (resolvedAddress === null) {
+    return res.status(400).send({ msg: `${req.body.address} is not a valid address` });
+  }
+
+  const recoveredAddress = utils.verifyMessage(
+    JSON.stringify({ action: 'remove', id: req.params.id }),
+    req.body.signature,
+  );
+  if (recoveredAddress !== resolvedAddress) {
+    return res.status(401).send({ msg: 'The signature is not valid for this address and data' });
+  }
+
+  const profile = await context.prisma.profile.findUnique({
+    where: {
+      address: resolvedAddress.toLowerCase(),
+    },
+  });
+  if (profile === null) {
+    return res.status(404).send({ msg: `There is no profile for the address ${req.body.address}` });
+  }
+
+  await context.prisma.featuredPOAP.delete({
+    where: {
+      poapTokenId_profileId: {
+        poapTokenId: req.params.id,
+        profileId: profile.id,
+      },
+    },
+  });
+
+  return res.status(200).send('DELETED');
 });
