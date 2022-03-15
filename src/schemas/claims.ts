a10cddfa649452ad282da0a5cf13e257caf5faca
Anthony Burzillo <burz@gitpoap.io>
2022-03-15 13:24:28 -0400

    Add endpoint to create claims for individuals

--- src/schemas/claims.ts
+++ src/schemas/claims.ts
@@ -5,3 +5,8 @@ export const ClaimGitPOAPSchema = z.object({
   claimIds: z.array(z.number()).nonempty(),
   signature: z.string(),
 });
+
+export const CreateGitPOAPClaimsSchema = z.object({
+  gitPOAPId: z.number(),
+  recipientGithubIds: z.array(z.number()).nonempty(),
+});
