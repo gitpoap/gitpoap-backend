d8953d555325a6a66b1bbec3ea5342809900dae4
Anthony Burzillo <burz@gitpoap.io>
2022-03-15 13:24:28 -0400

    Add endpoint to create claims for individuals

--- src/routes/claims.ts
+++ src/routes/claims.ts
@@ -1,4 +1,4 @@
-import { ClaimGitPOAPSchema } from '../schemas/claims';
+import { ClaimGitPOAPSchema, CreateGitPOAPClaimsSchema } from '../schemas/claims';
 import { Router } from 'express';
 import fetch from 'cross-fetch';
 import { context } from '../context';
@@ -7,7 +7,8 @@ import { resolveENS } from '../util';
 import { utils } from 'ethers';
 import jwt from 'express-jwt';
 import { AccessTokenPayload } from '../types';
-import { claimPOAP } from '../poap';
+import { claimPOAP, retrievePOAPEventInfo } from '../poap';
+import { getGithubUserById } from '../github';
 
 export const claimsRouter = Router();
 
@@ -149,5 +150,93 @@ claimsRouter.post(
       claimed: foundClaims,
       invalid: invalidClaims,
     });
+  },
+);
+
+claimsRouter.post(
+  '/create',
+  jwt({ secret: process.env.JWT_SECRET as string, algorithms: ['HS256'] }),
+  async function (req, res) {
+    if (!req.user) {
+      return res.status(401).send({ message: 'Invalid or missing Access Token' });
+    }
+    const userInfo = await context.prisma.authToken.findUnique({
+      where: {
+        id: (<AccessTokenPayload>req.user).authTokenId,
+      },
+      select: {
+        githubOAuthToken: true,
+      },
+    });
+    if (userInfo === null) {
+      return res.status(500).send({ msg: "Couldn't find your login" });
+    }
+
+    const schemaResult = CreateGitPOAPClaimsSchema.safeParse(req.body);
+
+    if (!schemaResult.success) {
+      return res.status(400).send({ issues: schemaResult.error.issues });
+    }
+
+    console.log(
+      `Received a request to create ${req.body.recipientGithubIds.length} claims for GitPOAP Id: ${req.body.gitPOAPId}`,
+    );
+
+    const gitPOAPData = await context.prisma.gitPOAP.findUnique({
+      where: {
+        id: req.body.gitPOAPId,
+      },
+    });
+    if (gitPOAPData === null) {
+      return res.status(404).send({ msg: `There is not GitPOAP with ID: ${req.body.gitPOAPId}` });
+    }
+
+    let notFound = [];
+    for (const githubId of req.body.recipientGithubIds) {
+      const githubUserInfo = await getGithubUserById(githubId, userInfo.githubOAuthToken);
+      if (githubUserInfo === null) {
+        console.log(`Github ID ${githubId} not found!`);
+        notFound.push(githubId);
+      }
+
+      // Ensure that we've created a user in our
+      // system for the claim
+      const user = await context.prisma.user.upsert({
+        where: {
+          githubId: githubId,
+        },
+        update: {
+          githubHandle: githubUserInfo.login,
+        },
+        create: {
+          githubId: githubId,
+          githubHandle: githubUserInfo.login,
+        },
+      });
+
+      await context.prisma.claim.create({
+        data: {
+          gitPOAP: {
+            connect: {
+              id: gitPOAPData.id,
+            },
+          },
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+        },
+      });
+    }
+
+    if (notFound.length > 0) {
+      return res.status(400).send({
+        msg: 'Some of the githubIds were not found',
+        ids: notFound,
+      });
+    } else {
+      return res.status(200).send('CREATED');
+    }
   },
 );
