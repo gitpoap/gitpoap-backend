0dd896f562441428475296708be87e537b15b562
Anthony Burzillo <burz@gitpoap.io>
2022-03-17 14:45:29 -0400

    Pad and uppercase the log level

--- src/logging.ts
+++ src/logging.ts
@@ -5,6 +5,11 @@ const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.errors({ stack: true }),
+    // Pad the levels and uppercase before they are colored
+    winston.format((info, opts) => {
+      info.level = info.level.toUpperCase().padStart(5);
+      return info;
+    })(),
     winston.format.colorize(),
     winston.format.printf(({ level, message, timestamp }) => {
       return `${timestamp} [gitpoap-backend] ${level}: ${message}`;
@@ -21,10 +26,10 @@ export function createScopedLogger(scope: string) {
   let scopeId = nextScopeId++;
 
   return {
-    debug: (msg: string) => logger.debug(`${scope} (${scopeId}): ${msg}`),
-    info: (msg: string) => logger.info(`${scope} (${scopeId}): ${msg}`),
-    warn: (msg: string) => logger.warn(`${scope} (${scopeId}): ${msg}`),
-    error: (msg: string) => logger.error(`${scope} (${scopeId}): ${msg}`),
+    debug: (msg: string) => logger.debug(`(${scopeId}) ${scope}: ${msg}`),
+    info: (msg: string) => logger.info(`(${scopeId}) ${scope}: ${msg}`),
+    warn: (msg: string) => logger.warn(`(${scopeId}) ${scope}: ${msg}`),
+    error: (msg: string) => logger.error(`(${scopeId}) ${scope}: ${msg}`),
   };
 }
 
