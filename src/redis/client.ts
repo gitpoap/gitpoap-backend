87a527a6ddc27e78fe942dad69b371f29bb3e3ee
Anthony Burzillo <burz@gitpoap.io>
2022-03-21 12:50:25 -0400

    Make redis client interface class-like

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -1,47 +1,48 @@
-import { createClient, RedisClientType, RedisModules, RedisScripts } from 'redis';
+import { createClient } from 'redis';
 import { REDIS_URL } from '../environment';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-export type RedisClient = RedisClientType<RedisModules, RedisScripts>;
+export type RedisClient = {
+  connect: () => Promise<any>;
+  setValue: (prefix: string, key: string, value: string, ttl?: number) => Promise<any>;
+  getValue: (prefix: string, key: string) => Promise<string | null>;
+  deleteKey: (prefix: string, key: string) => Promise<any>;
+  deletePrefix: (prefix: string) => Promise<any>;
+};
 
-export function createRedisClient(): RedisClient {
-  return createClient({
-    url: REDIS_URL,
-  });
-}
+//export type RedisClient = RedisClientType<RedisModules, RedisScripts>;
 
 function genKey(prefix: string, key: string) {
   return `${prefix}:${key}`;
 }
 
-// Note: ttl is in seconds
-export async function setValue(
-  client: RedisClient,
-  prefix: string,
-  key: string,
-  value: string,
-  ttl?: number,
-) {
-  if (ttl) {
-    return await client.set(genKey(prefix, key), value, { EX: ttl });
-  } else {
-    return await client.set(genKey(prefix, key), value);
-  }
-}
-
-export async function getValue(client: RedisClient, prefix: string, key: string) {
-  return await client.get(genKey(prefix, key));
-}
-
-export async function deleteKey(client: RedisClient, prefix: string, key: string) {
-  return await client.del(genKey(prefix, key));
-}
-
 const deleteScript = readFileSync(join(__dirname, 'deletePrefix.lua')).toString();
 
-export async function deletePrefix(client: RedisClient, prefix: string) {
-  return await client.eval(deleteScript, {
-    arguments: [`${prefix}:*`],
+export function createRedisClient(): RedisClient {
+  const client = createClient({
+    url: REDIS_URL,
   });
+
+  return {
+    connect: async () => {
+      return await client.connect();
+    },
+    setValue: async (prefix: string, key: string, value: string, ttl?: number) => {
+      if (ttl) {
+        return await client.set(genKey(prefix, key), value, { EX: ttl });
+      } else {
+        return await client.set(genKey(prefix, key), value);
+      }
+    },
+    getValue: async (prefix: string, key: string) => {
+      return await client.get(genKey(prefix, key));
+    },
+    deleteKey: async (prefix: string, key: string) => {
+      return await client.del(genKey(prefix, key));
+    },
+    deletePrefix: async (prefix: string) => {
+      return await client.eval(deleteScript, { arguments: [`${prefix}:*`] });
+    },
+  };
 }
