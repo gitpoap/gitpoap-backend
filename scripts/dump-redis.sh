f5bf0c5f19dcb73b411b53e3d382625eef067841
Anthony Burzillo <burz@gitpoap.io>
2022-03-21 16:29:54 -0400

    Add dump script for redis

---
+++ scripts/dump-redis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+source ./.env
+
+redis_cmd() {
+  redis-cli --no-auth-warning -u $REDIS_URL "$@"
+}
+
+for key in $(redis_cmd KEYS '*'); do
+  VALUE=$(redis_cmd GET $key)
+
+  echo "$key  ->  $VALUE"
+done
