f66a2c0f167a62c64fbb64d6cdc6e03358940aa8
Anthony Burzillo <burz@gitpoap.io>
2022-03-23 11:49:48 -0400

    Return an empty profile when not in DB

--- src/graphql/resolvers/profiles.ts
+++ src/graphql/resolvers/profiles.ts
@@ -1,16 +1,52 @@
-import { Arg, Ctx, Resolver, Query } from 'type-graphql';
-import { Profile } from '@generated/type-graphql';
+import { Arg, Ctx, Field, ObjectType, Resolver, Query } from 'type-graphql';
+import { FeaturedPOAP, Profile } from '@generated/type-graphql';
 import { Context } from '../../context';
 import { resolveENS } from '../../external/ens';
 import { createScopedLogger } from '../../logging';
 
+@ObjectType()
+class NullableProfile {
+  @Field(() => Number, { nullable: true })
+  id: number | null;
+
+  @Field()
+  address: string;
+
+  @Field(() => Date, { nullable: true })
+  createdAt: Date | null;
+
+  @Field(() => Date, { nullable: true })
+  updatedAt: Date | null;
+
+  @Field(() => String, { nullable: true })
+  bio: string | null;
+
+  @Field(() => String, { nullable: true })
+  bannerImageUrl: string | null;
+
+  @Field(() => String, { nullable: true })
+  name: string | null;
+
+  @Field(() => String, { nullable: true })
+  profileImageUrl: string | null;
+
+  @Field(() => String, { nullable: true })
+  twitterHandle: string | null;
+
+  @Field(() => String, { nullable: true })
+  personalSiteUrl: string | null;
+
+  @Field(() => [FeaturedPOAP])
+  featuredPOAPs: FeaturedPOAP[];
+}
+
 @Resolver(of => Profile)
 export class CustomProfileResolver {
-  @Query(returns => Profile, { nullable: true })
+  @Query(returns => NullableProfile, { nullable: true })
   async profileData(
     @Ctx() { prisma }: Context,
     @Arg('address') address: string,
-  ): Promise<Profile | null> {
+  ): Promise<NullableProfile | null> {
     const logger = createScopedLogger('GQL profileData');
 
     logger.info(`Request data for address: ${address}`);
@@ -21,11 +57,32 @@ export class CustomProfileResolver {
       return null;
     }
 
-    const result = await prisma.profile.findUnique({
+    let result: NullableProfile | null = await prisma.profile.findUnique({
       where: {
         address: resolvedAddress.toLowerCase(),
       },
+      include: {
+        featuredPOAPs: true,
+      },
     });
+
+    if (result === null) {
+      logger.debug(`Profile for ${address} not created yet, returning blank profile.`);
+
+      result = {
+        id: null,
+        address: resolvedAddress,
+        createdAt: null,
+        updatedAt: null,
+        bio: null,
+        bannerImageUrl: null,
+        name: null,
+        profileImageUrl: null,
+        twitterHandle: null,
+        personalSiteUrl: null,
+        featuredPOAPs: [],
+      };
+    }
 
     logger.debug(`Completed request data for address: ${address}`);
 
