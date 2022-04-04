f1ef67fca5011a0f273e01cc200b115e536d5121
Anthony Burzillo <burz@gitpoap.io>
2022-04-04 18:53:36 +0200

    Save progress on setting up redis stack

--- aws/gitpoap-backend-server/gitpoap_backend/gitpoap_backend_stack.py
+++ aws/gitpoap-backend-server/gitpoap_backend/gitpoap_backend_stack.py
@@ -1,27 +1,87 @@
 from aws_cdk import (
   Stack,
+  aws_ec2 as ec2,
   aws_ecr as ecr,
   aws_ecs as ecs,
-  aws_ec2 as ec2,
   aws_iam as iam,
+  aws_rds as rds,
+  aws_memorydb as memorydb,
 )
 from constructs import Construct
+import secrets
 
-class GitpoapBackendServerStack(Stack):
+def generate_and_save_passwords():
+  return {
+    'redis_password': secrets.token_urlsafe(25),
+  }
+
+class GitpoapBackendStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
+    passwords = generate_and_save_passwords()
 
-    # The code that defines your stack goes here
+    # Create the VPC
+    vpc = ec2.Vpc(self,
+                  "gitpoap-backend-vpc",
+                  max_azs=3)
+
+    # Create the redis cluster
+    redis_user = memorydb.CfnUser(self, 'gitpoap-redis-user',
+      user_name='gitpoap-redis-user',
+      access_string='on ~* &* +@all',
+      authentication_mode={
+        'Type': 'password',
+        'Passwords': [passwords['redis_password']],
+      },
+    )
+
+    redis_acl = memorydb.CfnACL(self, 'gitpoap-redis-acl',
+      acl_name='gitpoap-redis-acl',
+      user_names=[redis_user.user_name],
+    )
+
+    redis_securitygroup = ec2.SecurityGroup(self, 'gitpoap-redis-securitygroup',
+      vpc=vpc,
+      allow_all_outbound=True,
+      security_group_name='gitpoap-redis-securitygroup',
+    )
+
+    redis_client_securitygroup = ec2.SecurityGroup(self, 'gitpoap-redis-client-securitygroup',
+      vpc=vpc,
+      allow_all_outbound=True,
+      security_group_name='gitpoap-redis-client-securitygroup',
+    )
+
+    redis_securitygroup.add_ingress_rule(
+      peer=redis_client_securitygroup,
+      connection=ec2.Port.tcp(6379),
+    )
+
+    redis_subnet_group = memorydb.CfnSubnetGroup(self, 'gitpoap-redis-subnet-group',
+      subnet_ids=vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT).subnet_ids,
+      subnet_group_name='gitpoap-redis-subnet-group',
+    )
+
+    redis_cluster = memorydb.CfnCluster(self, 'gitpoap-redis-cluster',
+      cluster_name='gitpoap-redis-cluster',
+      acl_name=redis_acl.acl_name,
+      auto_minor_version_upgrade=True,
+      engine_version='6.2',
+      node_type='db.t4g.medium',
+      num_replicas_per_shard=1,
+      num_shards=1,
+      security_group_ids=[redis_securitygroup.security_group_id],
+      snapshot_retention_limit=3,
+      tls_enabled=True,
+      subnet_group_name=redis_subnet_group.subnet_group_name,
+    )
 
     # Create the ECR Repository
     ecr_repository = ecr.Repository(self,
                                     "gitpoap-backend-server-repository",
                                     repository_name="gitpoap-backend-server-repository")
 
-    # Create the ECS Cluster (and VPC)
-    vpc = ec2.Vpc(self,
-                  "gitpoap-backend-server-vpc",
-                  max_azs=3)
+    # Create the ECS Cluster
     cluster = ecs.Cluster(self,
                           "gitpoap-backend-server-cluster",
                           cluster_name="gitpoap-backend-server-cluster",
@@ -58,4 +118,5 @@ class GitpoapBackendServerStack(Stack):
                                  "gitpoap-backend-server-service",
                                  cluster=cluster,
                                  task_definition=task_definition,
+                                 security_groups=[redis_client_securitygroup],
                                  service_name="gitpoap-backend-server-service")
