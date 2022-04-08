d953ea52f1c5b33a4ea2e822e38328224f8f045f
Anthony Burzillo <burz@gitpoap.io>
2022-04-08 16:34:44 -0400

    Allow prefixes in dump-redis script

--- scripts/dump-redis.sh
+++ scripts/dump-redis.sh
@@ -8,7 +8,7 @@ redis_cmd() {
   redis-cli --no-auth-warning -u $REDIS_URL "$@"
 }
 
-for key in $(redis_cmd KEYS '*'); do
+for key in $(redis_cmd KEYS "$1*"); do
   VALUE=$(redis_cmd GET $key)
 
   echo "$key  ->  $VALUE"
