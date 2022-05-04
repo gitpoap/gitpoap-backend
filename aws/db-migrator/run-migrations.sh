2f7942fa88c901538e74b58ed3ac21477db969c1
Anthony Burzillo <burz@gitpoap.io>
2022-05-04 16:50:23 -0400

    Get db-migrator working locally

--- aws/db-migrator/run-migrations.sh
+++ aws/db-migrator/run-migrations.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-echo 'yay'
+set -e
 
-npx prisma migrate --help
+echo DB-MIGRATOR: Cloning gitpoap-backend...
+
+git clone "https://${GITHUB_OAUTH_TOKEN}@github.com/gitpoap/gitpoap-backend.git"
+
+cd gitpoap-backend
+
+echo DB-MIGRATOR: Installing node dependencies...
+
+yarn
+
+echo DB-MIGRATOR: Running deployment...
+
+npx prisma migrate deploy
+
+echo DB-MIGRATOR: Finished running migrations successfully
 
 exit 0
