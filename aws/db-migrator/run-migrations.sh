c4b53efa2a1f59f1b25f2bf84ab8ae009e85c514
Anthony Burzillo <burz@gitpoap.io>
2022-05-06 14:51:00 -0400

    Lookup VPC and security groups

--- aws/db-migrator/run-migrations.sh
+++ aws/db-migrator/run-migrations.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+if [ -z ${GITHUB_OAUTH_TOKEN+x} ]; then
+  echo 'Required ENV variable GITHUB_OAUTH_TOKEN is not set'
+  exit 1
+fi
+
+if [ -z ${DATABASE_URL+x} ]; then
+  echo 'Required ENV variable DATABASE_URL is not set'
+  exit 2
+fi
+
 echo DB-MIGRATOR: Cloning gitpoap-backend...
 
 git clone "https://${GITHUB_OAUTH_TOKEN}@github.com/gitpoap/gitpoap-backend.git"
