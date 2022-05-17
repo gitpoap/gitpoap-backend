cf7084093418429be893d1abfbf95e94a74ffa92
Anthony Burzillo <burz@gitpoap.io>
2022-05-17 22:18:22 +0000

    Parameterize stack names for staging and prod

--- aws/db-migrator/bin/db-migrator.js
+++ aws/db-migrator/bin/db-migrator.js
@@ -3,8 +3,10 @@
 const cdk = require('aws-cdk-lib');
 const { DbMigratorStack } = require('../lib/db-migrator-stack');
 
+const stackName = process.env.STAGE_TAG === '-staging' ? 'DbMigratorStagingStack' : 'DbMigratorStack';
+
 const app = new cdk.App();
-new DbMigratorStack(app, 'DbMigratorStack', {
+new DbMigratorStack(app, stackName, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
