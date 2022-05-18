03948cce15346aa204ed29b156992dbc3cb1e5c6
Anthony Burzillo <burz@gitpoap.io>
2022-05-17 21:48:39 -0400

    Switch to typescript

--- aws/db-migrator/lib/db-migrator-stack.ts
+++ aws/db-migrator/lib/db-migrator-stack.ts
@@ -1,35 +1,44 @@
-const { aws_ec2: ec2, aws_lambda: lambda, aws_logs: logs, Duration, Size, Stack, StorageUnit } = require('aws-cdk-lib');
-const path = require('path');
+import {
+  aws_ec2 as ec2,
+  aws_lambda as lambda,
+  aws_logs as logs,
+  App,
+  StackProps,
+  Duration,
+  Size,
+  Stack,
+} from 'aws-cdk-lib';
+import path from 'path';
 
-const VPC_ID = process.env.VPC_ID;
+const VPC_ID = process.env.VPC_ID || '';
 
 if (!VPC_ID) {
   console.log('Required ENV variable VPC_ID is not set');
   process.exit(1);
 }
 
-const SECURITY_GROUP_ID = process.env.SECURITY_GROUP_ID;
+const SECURITY_GROUP_ID = process.env.SECURITY_GROUP_ID || '';
 
 if (!SECURITY_GROUP_ID) {
   console.log('Required ENV variable SECURITY_GROUP_ID is not set');
   process.exit(2);
 }
 
-const GITHUB_OAUTH_TOKEN = process.env.GITHUB_OAUTH_TOKEN;
+const GITHUB_OAUTH_TOKEN = process.env.GITHUB_OAUTH_TOKEN || '';
 
 if (!GITHUB_OAUTH_TOKEN) {
   console.log('Required ENV variable GITHUB_OAUTH_TOKEN is not set');
   process.exit(3);
 }
 
-const REPO_BRANCH = process.env.REPO_BRANCH;
+const REPO_BRANCH = process.env.REPO_BRANCH || '';
 
 if (!REPO_BRANCH) {
   console.log('Required ENV variable REPO_BRANCH is not set');
   process.exit(4);
 }
 
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL || '';
 
 if (!DATABASE_URL) {
   console.log('Required ENV variable DATABASE_URL is not set');
@@ -43,14 +52,14 @@ console.log(`SECURITY_GROUP_ID = "${SECURITY_GROUP_ID}"`);
 console.log(`STAGE_TAG = "${STAGE_TAG}"`);
 console.log(`REPO_BRANCH = "${REPO_BRANCH}"`);
 
-class DbMigratorStack extends Stack {
+export class DbMigratorStack extends Stack {
   /**
    *
    * @param {Construct} scope
    * @param {string} id
    * @param {StackProps=} props
    */
-  constructor(scope, id, props) {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const vpc = ec2.Vpc.fromLookup(this, `gitpoap-backend${STAGE_TAG}-vpc`, {
@@ -74,7 +83,7 @@ class DbMigratorStack extends Stack {
         },
       }),
       vpc: vpc,
-      securityGroup: sg,
+      securityGroups: [sg],
       logRetention: logs.RetentionDays.ONE_DAY,
       environment: {
         DATABASE_URL,
@@ -85,5 +94,3 @@ class DbMigratorStack extends Stack {
     });
   }
 }
-
-module.exports = { DbMigratorStack };
