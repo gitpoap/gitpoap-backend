c8e38db935264e5e4809e4fec573e2d0486a6420
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-05-24 14:29:45 -0400

    improvements to resolveENS

--- src/external/ens.ts
+++ src/external/ens.ts
@@ -2,39 +2,46 @@ import { createScopedLogger } from '../logging';
 import { context } from '../context';
 import { ensRequestDurationSeconds } from '../metrics';
 import { SECONDS_PER_DAY } from '../constants';
+import { isAddress } from 'ethers/lib/utils';
 
 const ENS_RESOLVE_CACHE_PREFIX = 'ens#resolve';
 const ENS_RESOLVE_CACHE_TTL = 30 * SECONDS_PER_DAY; // 30 days
 
-export async function resolveENS(address: string): Promise<string | null> {
+/**
+ * Resolve an ENS name to an ETH address.
+ *
+ * @param ensName - the ENS name to resolve
+ * @returns the resolved ETH address associated with the ENS name or null
+ */
+export async function resolveENS(ensName: string): Promise<string | null> {
   const logger = createScopedLogger('resolveENS');
 
-  if (!address.endsWith('.eth')) {
-    logger.debug("Skipping lookup since address doesn't end with '.eth'");
-    return address;
+  if (!ensName.endsWith('.eth')) {
+    logger.debug("Skipping lookup since ensName doesn't end with '.eth'");
+    return ensName;
   }
 
-  const cacheResponse = await context.redis.getValue(ENS_RESOLVE_CACHE_PREFIX, address);
+  const cacheResponse = await context.redis.getValue(ENS_RESOLVE_CACHE_PREFIX, ensName);
 
   if (cacheResponse !== null) {
-    logger.debug(`Found ENS resolution of ${address} in cache`);
+    logger.debug(`Found ENS resolution of ${ensName} in cache`);
 
     return cacheResponse;
   }
 
-  logger.debug(`ENS resolution of ${address} not in cache`);
+  logger.debug(`ENS resolution of ${ensName} not in cache`);
 
   try {
     const endTimer = ensRequestDurationSeconds.startTimer('resolveName');
 
-    const resolvedAddress = await context.provider.resolveName(address);
+    const resolvedAddress = await context.provider.resolveName(ensName);
 
     endTimer();
 
-    if (address !== resolvedAddress) {
-      logger.debug(`Resolved ${address} to ${resolvedAddress}`);
+    if (ensName !== resolvedAddress) {
+      logger.debug(`Resolved ${ensName} to ${resolvedAddress}`);
       if (resolvedAddress === null) {
-        logger.debug(`${address} is not a valid address`);
+        logger.debug(`${ensName} is not a valid address`);
         return null;
       }
     }
@@ -42,7 +49,7 @@ export async function resolveENS(address: string): Promise<string | null> {
     // Set TTL to 30 days since we assume ENS will change infrequently
     context.redis.setValue(
       ENS_RESOLVE_CACHE_PREFIX,
-      address,
+      ensName,
       resolvedAddress,
       ENS_RESOLVE_CACHE_TTL,
     );
