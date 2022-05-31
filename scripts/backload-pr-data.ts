df7cb62828e09a78c32ff1ee758b6c9403772624
Anthony Burzillo <burz@gitpoap.io>
2022-05-31 13:42:56 -0400

    Hook up script to lib

--- scripts/backload-pr-data.ts
+++ scripts/backload-pr-data.ts
@@ -4,6 +4,7 @@ import 'reflect-metadata';
 import { backloadGithubPullRequestData } from '../src/lib/pullRequests';
 import { createScopedLogger, updateLogLevel } from '../src/logging';
 import minimist from 'minimist';
+import { context } from '../src/context';
 
 const main = async () => {
   const logger = createScopedLogger('main');
@@ -14,7 +15,15 @@ const main = async () => {
 
   updateLogLevel(argv['level']);
 
-  await backloadGithubPullRequestData(7);
+  const repos = await context.prisma.repo.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  for (const repo of repos) {
+    await backloadGithubPullRequestData(repo.id);
+  }
 };
 
 main();
