c6a8167dc2cd5609101181252ed86ba5623d19c6
Anthony Burzillo <burz@gitpoap.io>
2022-06-07 18:27:53 -0400

    Abstract out claims creation to library function

--- src/lib/ongoing.ts
+++ src/lib/ongoing.ts
@@ -10,6 +10,7 @@ import {
 } from '../metrics';
 import { extractMergeCommitSha } from './pullRequests';
 import { upsertUser } from './users';
+import { createNewClaimsForRepoPR } from './claims';
 
 // The number of pull requests to request in a single page (currently the maximum number)
 const PULL_STEP_SIZE = 100;
@@ -135,48 +136,7 @@ export async function checkForNewContributions(repo: RepoReturnType) {
         continue;
       }
 
-      const prCountData: { count: number }[] = await context.prisma.$queryRaw`
-        SELECT COUNT(id)
-        FROM "GithubPullRequest"
-        WHERE userId = ${user.id} AND repoId = ${repo.id}
-          AND date_part('year', githubMergedAt) = ${year}
-      `;
-      // There must be a result since we just created a PR
-      const prCount = prCountData[0].count;
-
-      for (const gitPOAP of repo.gitPOAPs) {
-        // Skip this GitPOAP if the threshold wasn't reached
-        if (prCount < gitPOAP.threshold) {
-          continue;
-        }
-
-        await context.prisma.claim.upsert({
-          where: {
-            gitPOAPId_userId: {
-              gitPOAPId: gitPOAP.id,
-              userId: user.id,
-            },
-          },
-          update: {},
-          create: {
-            gitPOAP: {
-              connect: {
-                id: gitPOAP.id,
-              },
-            },
-            user: {
-              connect: {
-                id: user.id,
-              },
-            },
-            pullRequestEarned: {
-              connect: {
-                id: githubPullRequest.id,
-              },
-            },
-          },
-        });
-      }
+      await createNewClaimsForRepoPR(user, repo, githubPullRequest);
     }
 
     ++page;
