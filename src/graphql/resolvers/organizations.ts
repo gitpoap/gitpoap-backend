9f37a753bd3fa1c40f81ee546c5b23c4eb0e28a9
Aldo Lamberti <jlamberti2015@gmail.com>
2022-06-10 15:36:09 -0700

    Create organizationData query

--- src/graphql/resolvers/organizations.ts
+++ src/graphql/resolvers/organizations.ts
@@ -1,11 +1,60 @@
-import { Arg, Ctx, Resolver, Query } from 'type-graphql';
+import { Arg, Ctx, Field, ObjectType, Resolver, Query } from 'type-graphql';
 import { Organization, OrganizationOrderByWithRelationInput } from '@generated/type-graphql';
 import { Context } from '../../context';
 import { createScopedLogger } from '../../logging';
 import { gqlRequestDurationSeconds } from '../../metrics';
+import { Prisma } from '@prisma/client';
+
+@ObjectType()
+class OrganizationData extends Organization {
+  @Field()
+  gitPOAPCount: number;
+  @Field()
+  projectCount: number;
+}
 
 @Resolver(of => Organization)
 export class CustomOrganizationResolver {
+  @Query(returns => OrganizationData, { nullable: true })
+  async organizationData(
+    @Ctx() { prisma }: Context,
+    @Arg('orgId', { defaultValue: null }) orgId?: number,
+    @Arg('orgName', { defaultValue: null }) orgName?: string,
+  ): Promise<OrganizationData | null> {
+    const logger = createScopedLogger('GQL organizationData');
+
+    logger.info(`Request data for organization: ${orgId ?? orgName}`);
+
+    const endTimer = gqlRequestDurationSeconds.startTimer('organizationData');
+
+    if (!orgId && !orgName) {
+      logger.warn('Either an "orgId" or an "orgName" must be provided');
+      endTimer({ success: 0 });
+      return null;
+    }
+
+    let results = await prisma.$queryRaw<OrganizationData[]>`
+      SELECT o.*, COUNT(DISTINCT g."id") AS "gitPOAPCount", COUNT(r.id) AS "projectCount"
+      FROM "Organization" as o
+      INNER JOIN "Repo" AS r ON r."organizationId" = o.id
+      INNER JOIN "GitPOAP" AS g ON g."repoId" = r.id
+      WHERE ${orgId ? Prisma.sql`o.id = ${orgId}` : Prisma.sql`o.name = ${orgName}`}
+      GROUP BY o.id
+    `;
+
+    if (results.length === 0) {
+      logger.warn(`Failed to find organization: ${orgId ?? orgName}`);
+      endTimer({ success: 0 });
+      return null;
+    }
+
+    logger.debug(`Completed request data for organization: ${orgId ?? orgName}`);
+
+    endTimer({ success: 1 });
+
+    return results[0];
+  }
+
   @Query(returns => [Organization], { nullable: true })
   async allOrganizations(
     @Ctx() { prisma }: Context,
