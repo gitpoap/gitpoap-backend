95bec8d09712759d11c3768ed6c13cdc44731376
Anthony Burzillo <burz@gitpoap.io>
2022-06-24 15:57:42 -0400

    Move common functions into library folder

---
+++ src/lib/repos.ts
@@ -0,0 +1,41 @@
+import { createScopedLogger } from '../logging';
+import { context } from '../context';
+import { RepoData } from './claims';
+
+export async function getRepoByName(owner: string, repo: string): Promise<RepoData | null> {
+  const logger = createScopedLogger('getRepoByName');
+
+  const result = await context.prisma.repo.findMany({
+    where: {
+      name: repo,
+      organization: {
+        name: owner,
+      },
+      gitPOAPs: {
+        every: {
+          ongoing: true,
+        },
+      },
+    },
+    select: {
+      id: true,
+      gitPOAPs: {
+        select: {
+          id: true,
+          year: true,
+          threshold: true,
+        },
+      },
+    },
+  });
+
+  if (result.length === 0) {
+    logger.error(`Couldn't find any repos in the DB named "${owner}/${repo}"`);
+    return null;
+  } else if (result.length > 1) {
+    logger.error(`Found multiple repos in DB named "${owner}/${repo}"`);
+    return null;
+  }
+
+  return result[0];
+}
