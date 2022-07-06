c5f68fe7289d422a4d15259a3ceab23bf1a21835
Anthony Burzillo <burz@gitpoap.io>
2022-07-06 19:10:16 -0400

    Add integration tests for claim and gitpoap GQL resolvers

---
+++ __tests__/integration/src/graphql/resolvers/claims.ts
@@ -0,0 +1,43 @@
+import { GraphQLClient, gql } from 'graphql-request';
+import { GH_IDS } from '../../../../../prisma/constants';
+
+describe('CustomClaimResolver', () => {
+  const client = new GraphQLClient('http://server:3001/graphql');
+
+  it('totalClaims', async () => {
+    const data = await client.request(gql`
+      {
+        totalClaims
+      }
+    `);
+
+    expect(data.totalClaims).toEqual(13);
+  });
+
+  it('lastMonthClaims', async () => {
+    const data = await client.request(gql`
+      {
+        lastMonthClaims
+      }
+    `);
+
+    expect(data.lastMonthClaims).toEqual(0);
+  });
+
+  it('userClaims', async () => {
+    const data = await client.request(gql`
+      {
+        userClaims(githubId: ${GH_IDS.jay}) {
+          claim {
+            id
+          }
+          event {
+            name
+          }
+        }
+      }
+    `);
+
+    expect(data.userClaims.length).toEqual(6);
+  });
+});
---
+++ prisma/constants.ts
@@ -0,0 +1,23 @@
+export const ADDRESSES = {
+  test1: '0xae95f7e7fb2fcf86148ef832faed2752ae5a358a' as const,
+  jay: '0xaE32D159BB3ABFcAdFaBE7aBB461C2AB4805596D'.toLowerCase(),
+  anthony: '0xAe95f7e7fb2FCF86148ef832FAeD2752Ae5A358a'.toLowerCase(),
+  anthony2: '0x206e554084BEeC98e08043397be63C5132Cc01A1'.toLowerCase(),
+  colfax: '0x56d389C4E07A48d429035532402301310B8143A0'.toLowerCase(),
+  vitalik: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'.toLowerCase(),
+  aldo: '0x02738d122e0970aAf8DEADf0c6A217a1923E1e99'.toLowerCase(),
+};
+
+export const GH_HANDLES = {
+  jay: 'jaypb1',
+  anthony: 'burz',
+  colfax: 'colfax23',
+  vitalik: 'vbuterin',
+  aldo: 'aldolamb',
+};
+
+export const GH_IDS = {
+  jay: 8076957,
+  anthony: 1555326,
+  aldo: 19416312,
+};
