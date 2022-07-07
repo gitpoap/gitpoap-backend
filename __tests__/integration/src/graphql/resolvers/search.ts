44015805b9ba1780d11a584ffc6eced11464d9b3
Anthony Burzillo <burz@gitpoap.io>
2022-07-07 19:34:13 -0400

    Add integration tests for Custom{Search,Repo}Resolver

---
+++ __tests__/integration/src/graphql/resolvers/search.ts
@@ -0,0 +1,68 @@
+import { GraphQLClient, gql } from 'graphql-request';
+import { ADDRESSES, GH_HANDLES } from '../../../../../prisma/constants';
+
+describe('CustomSearchResolver', () => {
+  const client = new GraphQLClient('http://server:3001/graphql');
+
+  it('search - usersByGithubHandle', async () => {
+    const data = await client.request(gql`
+      {
+        search(text: "${GH_HANDLES.aldo}") {
+          usersByGithubHandle {
+            id
+          }
+        }
+      }
+    `);
+
+    expect(data.search.usersByGithubHandle.length).toEqual(1);
+    expect(data.search.usersByGithubHandle[0].id).toEqual(5);
+  });
+
+  it('search - profilesByName', async () => {
+    const data = await client.request(gql`
+      {
+        search(text: "PB") {
+          profilesByName {
+            id
+          }
+        }
+      }
+    `);
+
+    expect(data.search.profilesByName.length).toEqual(1);
+    expect(data.search.profilesByName[0].id).toEqual(3);
+  });
+
+  it('search - profilesByAddress', async () => {
+    const data = await client.request(gql`
+      {
+        search(text: "${ADDRESSES.colfax}") {
+          profilesByAddress {
+            id
+          }
+        }
+      }
+    `);
+
+    expect(data.search.profilesByAddress.length).toEqual(1);
+    expect(data.search.profilesByAddress[0].id).toEqual(1);
+  });
+
+  it('search - profileByENS', async () => {
+    const data = await client.request(gql`
+      {
+        search(text: "burz.eth") {
+          profileByENS {
+            profile {
+              id
+            }
+          }
+        }
+      }
+    `);
+
+    expect(data.search.profileByENS).not.toEqual(null);
+    expect(data.search.profileByENS.profile.id).toEqual(4);
+  });
+});
