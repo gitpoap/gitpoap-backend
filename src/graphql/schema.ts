b6c55dcc894d3a71ac0f16cb055f97ccbf2104f7
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-07-08 19:06:47 -0400

    use auto-generated groupby & aggregate resolvers

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -13,34 +13,50 @@ import {
   FindUniqueGitPOAPResolver,
   FindFirstGitPOAPResolver,
   FindManyGitPOAPResolver,
+  GroupByGitPOAPResolver,
+  AggregateGitPOAPResolver,
   /* Auto-generated User Resolvers */
   FindUniqueUserResolver,
   FindFirstUserResolver,
   FindManyUserResolver,
+  GroupByUserResolver,
+  AggregateUserResolver,
   /* Auto-generated Profile Resolvers */
   FindUniqueProfileResolver,
   FindFirstProfileResolver,
   FindManyProfileResolver,
+  GroupByProfileResolver,
+  AggregateProfileResolver,
   /* Auto-generated Organization Resolvers */
   FindUniqueOrganizationResolver,
   FindFirstOrganizationResolver,
   FindManyOrganizationResolver,
+  GroupByOrganizationResolver,
+  AggregateOrganizationResolver,
   /* Auto-generated Repo Resolvers */
   FindUniqueRepoResolver,
   FindFirstRepoResolver,
   FindManyRepoResolver,
+  GroupByRepoResolver,
+  AggregateRepoResolver,
   /* Auto-generated Claim Resolvers */
   FindUniqueClaimResolver,
   FindFirstClaimResolver,
   FindManyClaimResolver,
+  GroupByClaimResolver,
+  AggregateClaimResolver,
   /* Auto-generated FeaturedPOAP Resolvers */
   FindUniqueFeaturedPOAPResolver,
   FindFirstFeaturedPOAPResolver,
   FindManyFeaturedPOAPResolver,
+  GroupByFeaturedPOAPResolver,
+  AggregateFeaturedPOAPResolver,
   /* Auto-generated GithubPullRequest Resolvers */
   FindUniqueGithubPullRequestResolver,
   FindFirstGithubPullRequestResolver,
   FindManyGithubPullRequestResolver,
+  GroupByGithubPullRequestResolver,
+  AggregateGithubPullRequestResolver,
 } from '@generated/type-graphql';
 import { CustomClaimResolver } from './resolvers/claims';
 import { CustomGitPOAPResolver } from './resolvers/gitpoaps';
@@ -63,34 +79,50 @@ const allResolvers: NonEmptyArray<Function> = [
   FindUniqueGitPOAPResolver,
   FindFirstGitPOAPResolver,
   FindManyGitPOAPResolver,
+  GroupByGitPOAPResolver,
+  AggregateGitPOAPResolver,
   /* Auto-generated User READ Resolvers */
   FindUniqueUserResolver,
   FindFirstUserResolver,
   FindManyUserResolver,
+  GroupByUserResolver,
+  AggregateUserResolver,
   /* Auto-generated Profile READ Resolvers */
   FindUniqueProfileResolver,
   FindFirstProfileResolver,
   FindManyProfileResolver,
+  GroupByProfileResolver,
+  AggregateProfileResolver,
   /* Auto-generated Organization READ Resolvers */
   FindUniqueOrganizationResolver,
   FindFirstOrganizationResolver,
   FindManyOrganizationResolver,
+  GroupByOrganizationResolver,
+  AggregateOrganizationResolver,
   /* Auto-generated Repo READ Resolvers */
   FindUniqueRepoResolver,
   FindFirstRepoResolver,
   FindManyRepoResolver,
+  GroupByRepoResolver,
+  AggregateRepoResolver,
   /* Auto-generated Claim READ Resolvers */
   FindUniqueClaimResolver,
   FindFirstClaimResolver,
   FindManyClaimResolver,
+  GroupByClaimResolver,
+  AggregateClaimResolver,
   /* Auto-generated FeaturedPOAP READ Resolvers */
   FindUniqueFeaturedPOAPResolver,
   FindFirstFeaturedPOAPResolver,
   FindManyFeaturedPOAPResolver,
+  GroupByFeaturedPOAPResolver,
+  AggregateFeaturedPOAPResolver,
   /* Auto-generated GithubPullRequest READ Resolvers */
   FindUniqueGithubPullRequestResolver,
   FindFirstGithubPullRequestResolver,
   FindManyGithubPullRequestResolver,
+  GroupByGithubPullRequestResolver,
+  AggregateGithubPullRequestResolver,
   /* ~~ Custom resolvers ~~ */
   CustomClaimResolver,
   CustomGitPOAPResolver,
