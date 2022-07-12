4ce0e96ef3c941ba7665c45d02b65dedf6fb6394
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-07-12 15:52:27 -0400

    create file in /external dir for mailchimp related functions

--- src/routes/subscribe.ts
+++ src/routes/subscribe.ts
@@ -3,12 +3,7 @@ import jwt from 'express-jwt';
 import { JWT_SECRET } from '../environment';
 import { createScopedLogger } from '../logging';
 import { httpRequestDurationSeconds } from '../metrics';
-import mailChimpClient from '@mailchimp/mailchimp_marketing';
-
-mailChimpClient.setConfig({
-  apiKey: 'e926f32160593e7dc66f7f632b03113d-us14',
-  server: 'us14',
-});
+import { addListMember } from '../external/mailchimp';
 
 export const subscribeRouter = Router();
 
@@ -17,9 +12,7 @@ subscribeRouter.post(
   jwt({ secret: JWT_SECRET as string, algorithms: ['HS256'] }),
   async function (req, res) {
     const logger = createScopedLogger('POST /subscribe');
-
     logger.debug(`Body: ${JSON.stringify(req.body)}`);
-
     const endTimer = httpRequestDurationSeconds.startTimer('POST', '/subscribe');
 
     if (!req.user) {
@@ -27,25 +20,18 @@ subscribeRouter.post(
       return res.sendStatus(401);
     } else {
       logger.info(`Request to subscribe ${req.body.email}`);
+      const listId = 'cc7ac358ee';
+      const mailChimpResponse = await addListMember(req.body.email, listId);
 
-      try {
-        /* Directly add email address to mailchimp list */
-        await mailChimpClient.lists.addListMember('cc7ac358ee', {
-          email_address: req.body.email,
-          status: 'subscribed',
-        });
-      } catch (err: any) {
-        /* On error, just log */
-        logger.error(err);
-        endTimer({ status: 200 });
-
-        return res.sendStatus(200);
+      if (mailChimpResponse === null) {
+        const message = `Failed to add email ${req.body.email} to MailChimp list ${listId}`;
+        logger.warn(message);
+        endTimer({ status: 400 });
+        return res.status(400).send({ msg: message });
       }
 
       logger.debug(`Completed request to subscribe ${req.body.email}`);
-
       endTimer({ status: 200 });
-
       res.sendStatus(200);
     }
   },
