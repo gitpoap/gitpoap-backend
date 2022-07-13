87cb7b174812a3aaa7eba7a33668be6216bb72df
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-07-13 18:26:39 -0400

    add prototype

--- src/graphql/resolvers/search.ts
+++ src/graphql/resolvers/search.ts
@@ -4,6 +4,7 @@ import { Context } from '../../context';
 import { resolveENS } from '../../external/ens';
 import { createScopedLogger } from '../../logging';
 import { gqlRequestDurationSeconds } from '../../metrics';
+import { Prisma } from '@prisma/client';
 
 @ObjectType()
 class ProfileWithENS {
@@ -27,6 +28,28 @@ class SearchResults {
 
   @Field(() => ProfileWithENS, { nullable: true })
   profileByENS: ProfileWithENS | null;
+}
+
+@ObjectType()
+class SearchResultItem {
+  @Field()
+  id: string;
+  @Field()
+  text: string;
+  @Field()
+  type: 'org' | 'profile' | 'repo';
+  @Field({ nullable: true })
+  href: string;
+  @Field({ nullable: true })
+  score: string;
+}
+
+@ObjectType()
+class SearchResultsV2 {
+  @Field(() => [SearchResultItem])
+  results: SearchResultItem[];
+  @Field()
+  total: number;
 }
 
 @Resolver()
@@ -91,6 +114,82 @@ export class CustomSearchResolver {
       profilesByName,
       profilesByAddress,
       profileByENS,
+    };
+  }
+
+  @Query(returns => SearchResultsV2)
+  async searchv2(@Ctx() { prisma }: Context, @Arg('text') text: string): Promise<SearchResultsV2> {
+    const logger = createScopedLogger('GQL search');
+    logger.info(`Request to search for "${text}"`);
+    const endTimer = gqlRequestDurationSeconds.startTimer('search');
+
+    const matchText = `%${text}%`;
+    const limit = 2;
+    const offset = 0;
+
+    /*
+    Things to add:
+    - search for profile by ens
+    - search for gitpoap by name
+    - search for gitpoap by description
+    */
+
+    const rawRecordsQuery = Prisma.sql`
+        SELECT
+          "Organization"."id",
+          "Organization"."name" AS text,
+          CONCAT('/gh/', "Organization"."name") AS href,
+          'org' AS type,
+          levenshtein("Organization"."name", ${matchText}) AS score
+          FROM "Organization"
+            WHERE "Organization"."name" ILIKE ${matchText}
+          UNION
+        SELECT
+          "Repo"."id",
+          "Repo"."name" AS text,
+          CONCAT('/gh/', "Organization"."name", '/', "Repo"."name") AS href,
+          'repo' AS type,
+          levenshtein("Repo"."name", ${matchText}) AS score
+          FROM "Repo" JOIN "Organization" ON "Repo"."organizationId" = "Organization"."id"
+            WHERE "Repo"."name" ILIKE ${matchText}
+          UNION
+        SELECT
+          "Profile"."id",
+          "Profile"."address" AS text,
+          CONCAT('/p/', "Profile"."address") AS href,
+          'profile' AS type,
+          levenshtein("Profile"."address", ${matchText}) AS score
+          FROM "Profile"
+            WHERE "address" ILIKE ${matchText}
+    `;
+
+    const query = Prisma.sql`
+      with raw_records as (
+        ${rawRecordsQuery}
+      )
+      SELECT *, COUNT(*) OVER() AS total_count
+        FROM raw_records
+        ORDER BY score ASC
+        LIMIT ${limit}
+        OFFSET ${offset}
+    `;
+
+    const totalCountQuery = Prisma.sql`
+      with raw_records as (
+        ${rawRecordsQuery}
+      )
+      SELECT COUNT(*)
+        FROM raw_records
+    `;
+
+    const results = await prisma.$queryRaw<SearchResultItem[]>(Prisma.sql`${query}`);
+    const total = await prisma.$queryRaw<[{ count: number }]>(Prisma.sql`${totalCountQuery}`);
+
+    logger.debug(`Completed request to search for "${text}"`);
+    endTimer({ success: 1 });
+    return {
+      results,
+      total: total[0].count,
     };
   }
 }
