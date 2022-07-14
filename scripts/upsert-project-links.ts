53d6468dd57f6925914904b14abfe46728ece74c
Anthony Burzillo <burz@gitpoap.io>
2022-07-14 18:19:00 -0400

    Remove uneeded name column from Project table

--- scripts/upsert-project-links.ts
+++ scripts/upsert-project-links.ts
@@ -4,7 +4,6 @@ import 'reflect-metadata';
 import { createScopedLogger, updateLogLevel } from '../src/logging';
 import minimist from 'minimist';
 import { context } from '../src/context';
-import { retrievePOAPEventInfo } from '../src/external/poap';
 
 async function nextGitPOAPWithMissingProjectId() {
   return await context.prisma.gitPOAP.findFirst({
@@ -13,7 +12,6 @@ async function nextGitPOAPWithMissingProjectId() {
     },
     select: {
       id: true,
-      poapEventId: true,
       repo: {
         select: {
           id: true,
@@ -48,55 +46,21 @@ const main = async () => {
     let projectId = gitPOAP.repo.projectId;
 
     if (projectId === null) {
-      const poapInfo = await retrievePOAPEventInfo(gitPOAP.poapEventId);
-      if (poapInfo === null) {
-        logger.error(
-          `Failed to look up POAP Event (id: ${gitPOAP.poapEventId} via the API for GitPOAP id: ${gitPOAP.id})`,
-        );
-        return;
-      }
+      projectId = (await context.prisma.project.create({ data: {} })).id;
 
-      let name: string;
-      if (poapInfo.name.indexOf('Hackathon') !== -1) {
-        // Convert: "GitPOAP: 2022 DevConnect Hackathon - GitPOAP Team Contributor"
-        // To: "GitPOAP Team"
-        const startIndex = poapInfo.name.indexOf('-') + 2;
+      logger.info(`Using a new Project (ID: ${projectId}) for GitPOAP ID ${gitPOAP.id}`);
 
-        name = poapInfo.name.substr(
-          startIndex,
-          poapInfo.name.indexOf('Contributor') - 1 - startIndex,
-        );
-      } else {
-        // first instance of "20" (e.g. in "2022" or "2015")
-        const startIndex = poapInfo.name.indexOf('20') + 5; // skip "2022 "
-
-        name = poapInfo.name.substr(
-          startIndex,
-          poapInfo.name.indexOf('Contributor') - 1 - startIndex,
-        );
-      }
-
-      logger.info(`Using project name "${name}" for GitPOAP "${poapInfo.name}"`);
-
-      projectId = (
-        await context.prisma.project.create({
-          data: {
-            name: name,
-          },
-        })
-      ).id;
+      await context.prisma.repo.update({
+        where: {
+          id: gitPOAP.repo.id,
+        },
+        data: {
+          projectId: projectId,
+        },
+      });
+    } else {
+      logger.info(`Using existing Project (ID: ${projectId}) for GitPOAP ID ${gitPOAP.id}`);
     }
-
-    logger.info(`Using Project id: ${projectId}`);
-
-    await context.prisma.repo.update({
-      where: {
-        id: gitPOAP.repo.id,
-      },
-      data: {
-        projectId: projectId,
-      },
-    });
 
     await context.prisma.gitPOAP.update({
       where: {
