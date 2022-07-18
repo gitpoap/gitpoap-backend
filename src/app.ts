63251b690c8c07b316b4e1373285e1ae2d44e4c1
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-07-18 18:37:06 -0400

    add back sentry to app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,27 @@ import { projectsRouter } from './routes/projects';
 import { organizationsRouter } from './routes/organizations';
 import { triggersRouter } from './routes/triggers';
 import { vitalsRouter } from './routes/vitals';
+import { NODE_ENV, SENTRY_DSN } from './environment';
+
+const initializeSentry = (app: Express) => {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: NODE_ENV,
+    /* Only send errors to sentry if env is production */
+    enabled: NODE_ENV === 'production',
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+  // RequestHandler creates a separate execution context using domains, so that every
+  // transaction/span/breadcrumb is attached to its own Hub instance
+  app.use(Sentry.Handlers.requestHandler());
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
+};
 
 export async function setupApp() {
   const app = express();
