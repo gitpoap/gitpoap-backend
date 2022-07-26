f78509945fe9ab2bfd92ceaa76d2da58a933c3b6
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-07-26 12:20:55 -0400

    create new constants and types files under v1 dir

---
+++ src/public-api/v1/types.ts
@@ -0,0 +1,13 @@
+export type GitPOAPResultType = {
+  gitPoapId: number;
+  gitPoapEventId: number;
+  poapTokenId: string;
+  poapEventId: number;
+  name: string;
+  year: number;
+  description: string;
+  imageUrl: string;
+  repositories: string[];
+  earnedAt: string;
+  mintedAt: string;
+};
