e28f6dbd5188c5f079f09b7d32b8e90d91f238da
Anthony Burzillo <burz@gitpoap.io>
2022-07-28 18:10:46 -0400

    Remove duplicated rows before making redeem codes unique

--- prisma/migrations/20220728220805_make_git_poap_id_and_code_unique_in_redeem_code/migration.sql
+++ prisma/migrations/20220728220805_make_git_poap_id_and_code_unique_in_redeem_code/migration.sql
@@ -4,5 +4,9 @@
   - A unique constraint covering the columns `[gitPOAPId,code]` on the table `RedeemCode` will be added. If there are existing duplicate values, this will fail.
 
 */
+
+-- Remove duplicated rows in RedeemCode (chooses the lowest ids to keep)
+DELETE FROM "RedeemCode" as a USING "RedeemCode" as b WHERE a.id > b.id AND a."gitPOAPId" = b."gitPOAPId" AND a.code = b.code;
+
 -- CreateIndex
 CREATE UNIQUE INDEX "RedeemCode_gitPOAPId_code_key" ON "RedeemCode"("gitPOAPId", "code");
