f61e2fbdd7c2801588e0840002a178b3d581b79e
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-08-08 20:29:50 -0400

    Create schema file for onboarding form

---
+++ src/schemas/onboarding.ts
@@ -0,0 +1,38 @@
+import { z } from 'zod';
+
+const MAX_FILE_SIZE = 500000;
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
+
+const ImageFileSchema = z
+  .any()
+  .refine(file => file?.size <= MAX_FILE_SIZE, `Max file size is 5MB.`)
+  .refine(
+    file => ACCEPTED_IMAGE_TYPES.includes(file?.mimetype),
+    '.jpg, .jpeg, .png and .webp files are accepted.',
+  );
+
+export const IntakeFormImageFilesSchema = z.array(ImageFileSchema);
+
+export const IntakeFormSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  notes: z.string(),
+  githubHandle: z.string(),
+  shouldGitPOAPDesign: z.enum(['true', 'false']),
+  isOneGitPOAPPerRepo: z.enum(['true', 'false']),
+  repos: z.string(),
+});
+
+export const IntakeFormReposSchema = z.array(
+  z.object({
+    full_name: z.string(),
+    githubRepoId: z.string(),
+    permissions: z.object({
+      admin: z.boolean(),
+      maintain: z.boolean().optional(),
+      push: z.boolean(),
+      triage: z.boolean().optional(),
+      pull: z.boolean(),
+    }),
+  }),
+);
