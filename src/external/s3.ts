2cb0257b437a9cf4ef2c4e1a4aeb6b7373be0a38
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-08-08 20:34:35 -0400

    create s3 client

---
+++ src/external/s3.ts
@@ -0,0 +1,43 @@
+import {
+  S3Client,
+  S3ClientConfig,
+  PutObjectCommand,
+  PutObjectCommandInput,
+} from '@aws-sdk/client-s3';
+import { fromIni } from '@aws-sdk/credential-provider-ini';
+import { AWS_PROFILE, NODE_ENV } from '../environment';
+
+type S3ClientConfigProfile = S3ClientConfig & {
+  buckets: Record<string, string>;
+};
+
+const S3_CONFIG_PROFILES: Record<'local' | 'prod', S3ClientConfigProfile> = {
+  local: {
+    region: 'us-east-2',
+    credentials: fromIni({ profile: AWS_PROFILE }),
+    buckets: {
+      intakeForm: 'intake-form-test',
+    },
+  },
+  prod: {
+    region: 'us-east-2',
+    buckets: {
+      intakeForm: 'intake-form',
+    },
+  },
+};
+
+export const s3configProfile = S3_CONFIG_PROFILES[NODE_ENV === 'local' ? 'local' : 'prod'];
+
+export const s3 = new S3Client(s3configProfile);
+
+export const uploadMulterFile = async (file: Express.Multer.File, bucket: string, key: string) => {
+  const params: PutObjectCommandInput = {
+    Bucket: bucket,
+    Key: key,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+  };
+
+  return await s3.send(new PutObjectCommand(params));
+};
