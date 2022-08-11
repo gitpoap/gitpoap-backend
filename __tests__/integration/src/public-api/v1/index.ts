e4ff4ff0e221a073f96484199b3bc9b405169fca
Anthony Burzillo <burz@gitpoap.io>
2022-08-11 14:09:37 -0400

    Move gitpoap-related routes to their own file

--- __tests__/integration/src/public-api/v1/index.ts
+++ __tests__/integration/src/public-api/v1/index.ts
@@ -1,93 +1,9 @@
 import fetch from 'cross-fetch';
-import { ADDRESSES, GH_HANDLES } from '../../../../prisma/constants';
+import { ADDRESSES, GH_HANDLES } from '../../../../../prisma/constants';
 import cheerio from 'cheerio';
-import { event29009, event3, event36571 } from '../../../../.dockerfiles/fake-poap-api/data';
+import { event29009, event3, event36571 } from '../../../../../.dockerfiles/fake-poap-api/data';
 
 const PUBLIC_API_URL = 'http://public-api-server:3122';
-
-describe('public-api/v1/poap/:poapTokenId/is-gitpoap', () => {
-  it('Returns false for a non-GitPOAP POAP ID', async () => {
-    const response = await fetch(`${PUBLIC_API_URL}/v1/poap/32423423/is-gitpoap`);
-
-    expect(response.status).toBeLessThan(400);
-
-    const data = await response.json();
-
-    expect(data.isGitPOAP).toEqual(false);
-  });
-
-  it('Returns true and the GitPOAP ID for a GitPOAP POAP ID', async () => {
-    const response = await fetch(`${PUBLIC_API_URL}/v1/poap/thunderdome/is-gitpoap`);
-
-    expect(response.status).toBeLessThan(400);
-
-    const data = await response.json();
-
-    expect(data.isGitPOAP).toEqual(true);
-    expect(data.gitPOAPId).toEqual(1);
-  });
-});
-
-describe('public-api/v1/poap-event/:poapEventId/is-gitpoap', () => {
-  it('Returns false for a non-GitPOAP POAP Event ID', async () => {
-    const response = await fetch(`${PUBLIC_API_URL}/v1/poap-event/2344444/is-gitpoap`);
-
-    expect(response.status).toBeLessThan(400);
-
-    const data = await response.json();
-
-    expect(data.isGitPOAP).toEqual(false);
-  });
-
-  it('Returns true and the GitPOAP Event ID for a GitPOAP POAP Event ID', async () => {
-    const response = await fetch(`${PUBLIC_API_URL}/v1/poap-event/1/is-gitpoap`);
-
-    expect(response.status).toBeLessThan(400);
-
-    const data = await response.json();
-
-    expect(data.isGitPOAP).toEqual(true);
-    expect(data.gitPOAPId).toEqual(1);
-  });
-});
-
-describe('public-api/v1/gitpoaps/:gitpoapId/addresses', () => {
-  it('Returns a 404 when the GitPOAP ID is not valid', async () => {
-    const response = await fetch(`${PUBLIC_API_URL}/v1/gitpoaps/990930423/addresses`);
-
-    expect(response.status).toEqual(404);
-  });
-
-  it('Returns the addresses of the owners when the GitPOAP ID is valid', async () => {
-    const response = await fetch(`${PUBLIC_API_URL}/v1/gitpoaps/1/addresses`);
-
-    expect(response.status).toBeLessThan(400);
-
-    const data = await response.json();
-
-    expect(data.addresses).not.toEqual(null);
-    expect(data.addresses.length).toEqual(2);
-    expect(data.addresses).toContain(ADDRESSES.test1);
-    expect(data.addresses).toContain(ADDRESSES.jay);
-  });
-});
-
-describe('public-api/v1/gitpoaps/addresses', () => {
-  it('Returns all addresses that have claimed GitPOAPs', async () => {
-    const response = await fetch(`${PUBLIC_API_URL}/v1/gitpoaps/addresses`);
-
-    expect(response.status).toBeLessThan(400);
-
-    const data = await response.json();
-
-    expect(data.addresses).not.toEqual(null);
-    expect(data.addresses.length).toEqual(4);
-    expect(data.addresses).toContain(ADDRESSES.test1);
-    expect(data.addresses).toContain(ADDRESSES.jay);
-    expect(data.addresses).toContain(ADDRESSES.colfax);
-    expect(data.addresses).toContain(ADDRESSES.anthony2);
-  });
-});
 
 describe('public-api/v1/address/:address/gitpoaps', () => {
   it('Returns 400 when address is invalid', async () => {
