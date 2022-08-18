30e99976a7b49377a0f3657efe77a39dbde06e1f
Anthony Burzillo <burz@gitpoap.io>
2022-08-18 13:22:19 -0400

    Add unit tests for enablement endpoints

--- __tests__/unit/src/routes/projects.ts
+++ __tests__/unit/src/routes/projects.ts
@@ -16,7 +16,7 @@ const authTokenId = 4;
 const githubId = 232444;
 const githubOAuthToken = 'foobar34543';
 const githubHandle = 'b-burz';
-const projectId = 1;
+const projectId = 234;
 const githubRepoIds = [2];
 
 describe('POST /projects/add-repos', () => {
@@ -122,5 +122,77 @@ describe('POST /projects/add-repos', () => {
     );
     expect(mockedBackloadGithubPullRequestData).toHaveBeenCalledTimes(1);
     expect(mockedBackloadGithubPullRequestData).toHaveBeenCalledWith(repoId);
+  });
+});
+
+describe('PUT /projects/enable/:id', () => {
+  it('Fails with no Access Token provided', async () => {
+    const result = await request(await setupApp())
+      .put(`/projects/enable/${projectId}`).send();
+
+    expect(result.statusCode).toEqual(400);
+  });
+
+  it('Fails with non-admin Access Token provided', async () => {
+    contextMock.prisma.authToken.findUnique.mockResolvedValue({ githubOAuthToken } as any);
+
+    const authTokens = generateAuthTokens(234, 4, githubId, githubHandle);
+
+    const result = await request(await setupApp())
+      .put(`/projects/enable/${projectId}`)
+      .set('Authorization', `Bearer ${authTokens.accessToken}`)
+      .send();
+
+    expect(result.statusCode).toEqual(401);
+
+    expect(contextMock.prisma.authToken.findUnique).toHaveBeenCalledTimes(1);
+  });
+
+  it('Returns a 404 when the Project is not found', async () => {
+    contextMock.prisma.authToken.findUnique.mockResolvedValue({ githubOAuthToken } as any);
+    contextMock.prisma.project.findUnique.mockResolvedValue(null);
+
+    const authTokens = generateAuthTokens(234, 4, ADMIN_GITHUB_IDS[0], githubHandle);
+
+    const result = await request(await setupApp())
+      .put(`/projects/enable/${projectId}`)
+      .set('Authorization', `Bearer ${authTokens.accessToken}`)
+      .send();
+
+    expect(result.statusCode).toEqual(404);
+
+    expect(contextMock.prisma.authToken.findUnique).toHaveBeenCalledTimes(1);
+    expect(contextMock.prisma.project.findUnique).toHaveBeenCalledTimes(1);
+    expect(contextMock.prisma.project.findUnique).toHaveBeenCalledWith({
+      where: { id: projectId },
+      select: { id: true },
+    });
+  });
+
+  it('Enables all GitPOAPs when the Project is found', async () => {
+    contextMock.prisma.authToken.findUnique.mockResolvedValue({ githubOAuthToken } as any);
+    contextMock.prisma.project.findUnique.mockResolvedValue({ id: projectId } as any);
+
+    const authTokens = generateAuthTokens(234, 4, ADMIN_GITHUB_IDS[0], githubHandle);
+
+    const result = await request(await setupApp())
+      .put(`/projects/enable/${projectId}`)
+      .set('Authorization', `Bearer ${authTokens.accessToken}`)
+      .send();
+
+    expect(result.statusCode).toEqual(200);
+
+    expect(contextMock.prisma.authToken.findUnique).toHaveBeenCalledTimes(1);
+    expect(contextMock.prisma.project.findUnique).toHaveBeenCalledTimes(1);
+    expect(contextMock.prisma.project.findUnique).toHaveBeenCalledWith({
+      where: { id: projectId },
+      select: { id: true },
+    });
+
+    expect(contextMock.prisma.gitPOAP.updateMany).toHaveBeenCalledTimes(1);
+    expect(contextMock.prisma.gitPOAP.updateMany).toHaveBeenCalledWith({
+      where: { projectId },
+      data: { isEnabled: true },
+    });
   });
 });
