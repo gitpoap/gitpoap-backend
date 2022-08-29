4179c271765a0809dc483ca30c96ac256f9c0a4f
Anthony Burzillo <burz@gitpoap.io>
2022-08-29 17:35:08 -0400

    Refactor splitUsersPOAPs

--- __tests__/integration/src/public-api/v1/routes/gitpoaps.ts
+++ __tests__/integration/src/public-api/v1/routes/gitpoaps.ts
@@ -58,7 +58,6 @@ describe('public-api/v1/gitpoaps/addresses', () => {
     expect(data.addresses).toContain(ADDRESSES.jay);
     expect(data.addresses).toContain(ADDRESSES.colfax);
     expect(data.addresses).toContain(ADDRESSES.burz2);
-    expect(data.addresses).toContain(ADDRESSES.anthony2);
     expect(data.addresses).toContain(ADDRESSES.aldo);
   });
 });
