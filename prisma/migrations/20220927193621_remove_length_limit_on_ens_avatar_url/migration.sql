6bd32f02224bbc463b524e567d36ceb66687e2d8
Anthony Burzillo <burz@gitpoap.io>
2022-09-27 15:37:12 -0400

    Remove length limit on ens avatar urls

---
+++ prisma/migrations/20220927193621_remove_length_limit_on_ens_avatar_url/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Address" ALTER COLUMN "ensAvatarImageUrl" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Profile" ALTER COLUMN "oldEnsAvatarImageUrl" SET DATA TYPE TEXT;
