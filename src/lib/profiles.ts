56d360d442492e6830de14b75b2eb87f652cc7d2
Anthony Burzillo <burz@gitpoap.io>
2022-09-28 12:37:38 -0400

    Upsert non-existent profiles within profileData

--- src/lib/profiles.ts
+++ src/lib/profiles.ts
@@ -1,15 +1,19 @@
 import { context } from '../context';
+import { Profile } from '@prisma/client';
 
-export async function upsertProfile(address: string) {
+export async function upsertProfile(address: string, oldEnsName?: string | null): Promise<Profile> {
   const addressLower = address.toLowerCase();
 
-  await context.prisma.profile.upsert({
+  return await context.prisma.profile.upsert({
     where: {
       oldAddress: addressLower,
     },
-    update: {},
+    update: {
+      oldEnsName,
+    },
     create: {
       oldAddress: addressLower,
+      oldEnsName,
     },
   });
 }
