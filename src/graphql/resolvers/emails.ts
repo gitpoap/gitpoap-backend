e9a7a37b9e226d264d2ee1eb3872e1e2975e6a56
Aldo Lamberti <jlamberti2015@gmail.com>
2022-10-03 18:05:28 -0400

    Change type to type-graphql

--- src/graphql/resolvers/emails.ts
+++ src/graphql/resolvers/emails.ts
@@ -1,15 +1,28 @@
 import { Email } from '@generated/type-graphql';
-import { Arg, Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Field, ObjectType, Query, Resolver } from 'type-graphql';
 
 import { Context } from '../../context';
 import { createScopedLogger } from '../../logging';
 import { gqlRequestDurationSeconds } from '../../metrics';
 
-type EmailResponse = Pick<Email, 'id' | 'emailAddress' | 'isValidated' | 'tokenExpiresAt'>;
+@ObjectType()
+class EmailResponse {
+  @Field(() => Number)
+  id: number;
+
+  @Field(() => String)
+  emailAddress: string;
+
+  @Field(() => Boolean)
+  isValidated: boolean;
+
+  @Field(() => Date)
+  tokenExpiresAt: Date;
+}
 
 @Resolver(of => Email)
 export class CustomEmailResolver {
-  @Query(returns => Email, { nullable: true })
+  @Query(returns => EmailResponse, { nullable: true })
   async userEmail(
     @Ctx() { prisma }: Context,
     @Arg('ethAddress') ethAddress: string,
