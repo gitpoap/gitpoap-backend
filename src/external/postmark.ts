64eb2e3804498c52e1b31d5a4f51ed32e718c073
Aldo Lamberti <jlamberti2015@gmail.com>
2022-10-05 15:26:29 -0400

    Move verification email to /external/postmark Create constants of defaults Update status code from 400 to 401

--- src/external/postmark.ts
+++ src/external/postmark.ts
@@ -2,3 +2,19 @@ import { ServerClient } from 'postmark';
 import { POSTMARK_SERVER_TOKEN } from '../environment';
 
 export const postmarkClient = new ServerClient(POSTMARK_SERVER_TOKEN);
+
+export const sendVerificationEmail = async (email: string, activeToken: string) => {
+  postmarkClient.sendEmailWithTemplate({
+    From: 'team@gitpoap.io',
+    To: email,
+    TemplateAlias: 'verify-email',
+    TemplateModel: {
+      product_url: 'gitpoap.io',
+      product_name: 'GitPOAP',
+      token: activeToken,
+      support_email: 'team@gitpoap.io',
+      company_name: 'MetaRep Labs Inc',
+      company_address: 'One Broadway, Cambridge MA 02142',
+    },
+  });
+};
