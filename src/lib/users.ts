89e3da8b7a934e9d1d4fdaf52b2341fd314e2ac7
Anna Burzillo <burz@gitpoap.io>
2022-10-05 19:25:36 -0400

    Update GitHub login flow

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -2,41 +2,27 @@ import { context } from '../context';
 import { User } from '@generated/type-graphql';
 import { createScopedLogger } from '../logging';
 
-// Temporarily wrap the upserts in a try-catch while
-// we have an issue open in the prisma repo
-export async function upsertUser(id: number, handle: string): Promise<User> {
+export async function upsertUser(
+  githubId: number,
+  githubHandle: string,
+  githubOAuthToken?: string,
+): Promise<User> {
   const logger = createScopedLogger('upsertUser');
 
-  logger.info(`Attempting to upsert GitHub user ${handle}`);
+  logger.info(`Attempting to upsert GitHub user ${githubHandle}`);
 
-  try {
-    return await context.prisma.user.upsert({
-      where: {
-        githubId: id,
-      },
-      update: {
-        githubHandle: handle,
-      },
-      create: {
-        githubId: id,
-        githubHandle: handle,
-      },
-    });
-  } catch (err) {
-    logger.warn(`Caught error: ${err}`);
-
-    const user = await context.prisma.user.findUnique({
-      where: {
-        githubId: id,
-      },
-    });
-
-    if (user === null) {
-      const msg = `Failed to upsert GitHub user ${handle} but the user doesn't exist!`;
-      logger.error(msg);
-      throw Error(msg);
-    }
-
-    return user;
-  }
+  return await context.prisma.user.upsert({
+    where: {
+      githubId,
+    },
+    update: {
+      githubHandle,
+      githubOAuthToken,
+    },
+    create: {
+      githubId,
+      githubHandle,
+      githubOAuthToken,
+    },
+  });
 }
