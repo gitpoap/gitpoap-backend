f1b2f9ae147b4ee0e23f634ce7104ccd9d121438
Anna Burzillo <burz@gitpoap.io>
2022-10-05 19:52:20 -0400

    Update logic for token refreshes

--- src/routes/github.ts
+++ src/routes/github.ts
@@ -1,10 +1,7 @@
 import { Router } from 'express';
-import { verify } from 'jsonwebtoken';
 import { context } from '../context';
-import { RequestAccessTokenSchema, RefreshAccessTokenSchema } from '../schemas/github';
-import { RefreshTokenPayload } from '../types/tokens';
+import { RequestAccessTokenSchema } from '../schemas/github';
 import { requestGithubOAuthToken, getGithubCurrentUserInfo } from '../external/github';
-import { JWT_SECRET } from '../environment';
 import { createScopedLogger } from '../logging';
 import { httpRequestDurationSeconds } from '../metrics';
 import { generateAuthTokens } from '../lib/authTokens';
@@ -99,98 +96,4 @@ githubRouter.post('/', jwtWithAddress(), async function (req, res) {
   endTimer({ status: 200 });
 
   return res.status(200).send(userAuthTokens);
-});
-
-githubRouter.post('/refresh', async function (req, res) {
-  const logger = createScopedLogger('POST /github/refresh');
-
-  logger.debug(`Body: ${JSON.stringify(req.body)}`);
-
-  const endTimer = httpRequestDurationSeconds.startTimer('POST', '/github/refresh');
-
-  const schemaResult = RefreshAccessTokenSchema.safeParse(req.body);
-  if (!schemaResult.success) {
-    logger.warn(
-      `Missing/invalid body fields in request: ${JSON.stringify(schemaResult.error.issues)}`,
-    );
-    endTimer({ status: 400 });
-    return res.status(400).send({ issues: schemaResult.error.issues });
-  }
-
-  logger.info('Request to refresh AuthToken');
-
-  const { token } = req.body;
-  let payload: RefreshTokenPayload;
-  try {
-    payload = <RefreshTokenPayload>verify(token, JWT_SECRET);
-  } catch (err) {
-    logger.warn('The refresh token is invalid');
-    endTimer({ status: 401 });
-    return res.status(401).send({ message: 'The refresh token is invalid' });
-  }
-
-  const authToken = await context.prisma.authToken.findUnique({
-    where: {
-      id: payload.authTokenId,
-    },
-    include: {
-      user: {
-        select: {
-          githubHandle: true,
-        },
-      },
-    },
-  });
-
-  if (authToken === null) {
-    logger.warn('The refresh token is invalid');
-    endTimer({ status: 401 });
-    return res.status(401).send({ message: 'The refresh token is invalid' });
-  }
-
-  // If someone is trying to use an old generation of the refresh token, we must
-  // consider the lineage to be tainted, and therefore purge it completely
-  // (user will need to log back in via GitHub).
-  if (payload.generation !== authToken.generation) {
-    logger.warn(`GitHub user ${authToken.githubId} had a refresh token reused.`);
-
-    try {
-      await context.prisma.authToken.delete({
-        where: {
-          id: authToken.id,
-        },
-      });
-    } catch (err) {
-      logger.warn(`Tried to delete an AuthToken that was already deleted: ${err}`);
-    }
-
-    endTimer({ status: 401 });
-
-    return res.status(401).send({ message: 'The refresh token has already been used' });
-  }
-
-  const nextGeneration = authToken.generation + 1;
-  await context.prisma.authToken.update({
-    where: {
-      id: authToken.id,
-    },
-    data: {
-      generation: nextGeneration,
-    },
-  });
-
-  logger.debug('Completed request to refresh AuthToken');
-
-  endTimer({ status: 200 });
-
-  return res
-    .status(200)
-    .json(
-      generateAuthTokens(
-        authToken.id,
-        nextGeneration,
-        payload.githubId,
-        authToken.user.githubHandle,
-      ),
-    );
 });
