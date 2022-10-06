d4841e61a6fcd3bc9dd85c1fe2c79d36b58af77a
Anna Burzillo <burz@gitpoap.io>
2022-10-05 22:44:03 -0400

    Add route to login

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,4 +1,10 @@
 import { z } from 'zod';
+import { SignatureSchema } from './signature';
+
+export const CreateAccessTokenSchema = z.object({
+  address: z.string().nonempty(),
+  signature: SignatureSchema,
+});
 
 export const RefreshAccessTokenSchema = z.object({
   token: z.string().nonempty(),
