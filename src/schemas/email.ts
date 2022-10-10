a4344e7cdd011e8721be648bea434ece5e316753
Aldo Lamberti <jlamberti2015@gmail.com>
2022-10-10 11:40:45 -0500

    Update routes to reflect recent auth changes

--- src/schemas/email.ts
+++ src/schemas/email.ts
@@ -1,19 +1,5 @@
 import { z } from 'zod';
 
-import { SignatureSchema } from './signature';
-
 export const AddEmailSchema = z.object({
-  address: z.string().nonempty(),
   emailAddress: z.string().nonempty(),
-  signature: SignatureSchema,
-});
-
-export const RemoveEmailSchema = z.object({
-  address: z.string().nonempty(),
-  id: z.number(),
-  signature: SignatureSchema,
-});
-
-export const ValidateEmailSchema = z.object({
-  activeToken: z.string().nonempty(),
 });
