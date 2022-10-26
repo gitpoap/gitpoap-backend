1fcc4c05623b236fc0339759f68f510029cbfdfc
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-10-26 17:35:30 -0400

    chore: address PR comments

--- src/types/utility.ts
+++ src/types/utility.ts
@@ -1,3 +1,8 @@
+/**
+ * This is a utility type that allows for the creation of a Partial types that span
+ * more than just the first level of object fields. If a field is an object, it will
+ * recursively create a Partial of that object.
+ */
 export type DeepPartial<T> = T extends object
   ? {
       [P in keyof T]?: DeepPartial<T[P]>;
