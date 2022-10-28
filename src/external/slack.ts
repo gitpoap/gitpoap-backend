cf6335b42eda35877724b185ce68d48efc204444
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-10-28 16:58:54 -0400

    feat: create slack client lib & dispatch message when a claim is made

---
+++ src/external/slack.ts
@@ -0,0 +1,42 @@
+import { WebClient } from '@slack/web-api';
+import { SLACK_TOKEN } from '../environment';
+import { createScopedLogger } from '../logging';
+
+const slackClient = new WebClient(SLACK_TOKEN);
+
+type SlackOrgs = 'gitpoap';
+
+type SlackChannels = {
+  [org in SlackOrgs]: Record<string, string>;
+};
+
+const CHANNELS: SlackChannels = {
+  gitpoap: {
+    alerts: 'C049AGT4BHN',
+  },
+};
+
+const sendSlackMessage = async (message: string, channelId: string) => {
+  const logger = createScopedLogger('sendSlackMessage');
+  try {
+    logger.debug(`Sending slack message to channel: ${channelId}`);
+    await slackClient.chat.postMessage({
+      channel: channelId,
+      text: message,
+    });
+  } catch (e) {
+    logger.error(`${e}`);
+  }
+};
+
+const sendInternalMessage = async (message: string) =>
+  await sendSlackMessage(message, CHANNELS.gitpoap.alerts);
+
+export const sendInternalClaimMessage = async (
+  claims: number[],
+  githubHandle: string,
+  address: string,
+) => {
+  const msg = `💸 Claimed GitPOAP(s) ${claims} for GitHub user ${githubHandle} with address ${address} 🥳`;
+  await sendInternalMessage(msg);
+};
