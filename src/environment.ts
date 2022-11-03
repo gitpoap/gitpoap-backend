8008ee5c8d54cf3fc36ba349f9cd453da5b74757
tyler415git <tyler@gitpoap.io>
2022-11-03 10:52:18 -0700

    fix: pr comments

--- src/environment.ts
+++ src/environment.ts
@@ -30,3 +30,6 @@ export const MAILCHIMP_API_KEY = env.get('MAILCHIMP_API_KEY').required().asStrin
 export const SENTRY_DSN = env.get('SENTRY_DSN').asString();
 export const POSTMARK_SERVER_TOKEN = env.get('POSTMARK_SERVER_TOKEN').required().asString();
 export const SLACK_TOKEN = env.get('SLACK_TOKEN').required().asString();
+
+export const GITPOAP_ROOT_URL = env.get('GITPOAP_ROOT_URL').required().asString();
+export const AWS_S3_ROOT_URL = env.get('AWS_S3_ROOT_URL').required().asString();
