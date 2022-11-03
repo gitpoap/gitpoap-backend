2da255c419d41653c0b94e9b98dae931a07e450a
tyler415git <tyler@gitpoap.io>
2022-11-03 09:32:48 -0700

    feat: send cg request submission confirmation email

--- src/types/gitpoaps.ts
+++ src/types/gitpoaps.ts
@@ -1,3 +1,12 @@
+export type CGRequestEmailForm = {
+  email: string;
+  name: string;
+  description: string;
+  imageKey: string;
+  organizationName?: string;
+  organizationId?: number;
+};
+
 export type GitPOAPContributors = {
   githubHandles: string[];
   ethAddresses: string[];
