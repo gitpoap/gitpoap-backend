628c204b725cbb93f612e69360733138ab4a2b57
tyler415git <tyler@gitpoap.io>
2022-11-08 11:40:39 -0800

    fix: pr comments

--- src/external/slack.ts
+++ src/external/slack.ts
@@ -4,8 +4,7 @@ import { SLACK_TOKEN } from '../environment';
 import { createScopedLogger } from '../logging';
 import { FoundClaim } from '../types/claims';
 import { shortenAddress } from '../lib/addresses';
-import { isProd } from '../environment';
-import { GITPOAP_ROOT_URL, GITPOAP_DEV_ROOT_URL } from '../constants';
+import { GITPOAP_ROOT_URL, GITPOAP_DEV_ROOT_URL, IS_PROD } from '../constants';
 
 const slackClient = new WebClient(SLACK_TOKEN);
 
@@ -24,14 +23,14 @@ const CHANNELS: SlackChannels = {
   },
 };
 
-const GITPPOAP_URL = isProd ? GITPOAP_ROOT_URL : GITPOAP_DEV_ROOT_URL;
+const GITPPOAP_URL = IS_PROD ? GITPOAP_ROOT_URL : GITPOAP_DEV_ROOT_URL;
 
 const sendSlackMessage = async (message: string, channelId: string) => {
   const logger = createScopedLogger('sendSlackMessage');
   try {
     logger.debug(`Sending slack message to channel: ${channelId}`);
     await slackClient.chat.postMessage({
-      channel: channelId,
+      channel: IS_PROD ? channelId : CHANNELS.gitpoap.devAlerts,
       text: message,
     });
   } catch (e) {
@@ -41,10 +40,10 @@ const sendSlackMessage = async (message: string, channelId: string) => {
 
 const sendInternalMessage = async (message: string) =>
   await sendSlackMessage(message, CHANNELS.gitpoap.alerts);
-const sendDevInternalMessage = async (message: string) =>
-  await sendSlackMessage(message, CHANNELS.gitpoap.devAlerts);
+
 const sendOnboardingMessage = async (message: string) =>
   await sendSlackMessage(message, CHANNELS.gitpoap.onboardingAlerts);
+
 const sendGitPOAPRequestMessage = async (message: string) =>
   await sendSlackMessage(message, CHANNELS.gitpoap.gitpoapRequestAlerts);
 
@@ -63,11 +62,7 @@ export const sendInternalClaimMessage = async (
     list += `\n* <${GITPPOAP_URL}/gp/${claim.gitPOAPId}|[GitPOAP ID ${claim.gitPOAPId}]: ${claim.gitPOAPName}>`;
   }
 
-  if (isProd) {
-    await sendInternalMessage(topMsg + list);
-  } else {
-    await sendDevInternalMessage(topMsg + list);
-  }
+  await sendInternalMessage(topMsg + list);
 };
 
 export const sentInternalGitPOAPRequestMessage = async ({
@@ -77,19 +72,13 @@ export const sentInternalGitPOAPRequestMessage = async ({
 }: GitPOAPRequest) => {
   const gitPOAPRequestLink = GITPPOAP_URL + `/admin/gitpoap/requests?search=${id}`;
   const msg = `📬 Received request to create GitPOAP - Request ID: ${id}, Name: ${name} Description:${description}. View ${gitPOAPRequestLink} to view more information`;
-  if (isProd) {
-    await sendGitPOAPRequestMessage(msg);
-  } else {
-    await sendDevInternalMessage(msg);
-  }
+
+  await sendGitPOAPRequestMessage(msg);
 };
 
 export const sentInternalOnboardingMessage = async ({ id, name, description }: GitPOAP) => {
   const gitPOAPRequestLink = GITPPOAP_URL + `/gp/${id}`;
   const msg = `📬 Received to create GitPOAP - GitPOAP ID: ${id}, Name: ${name} Description:${description}. View ${gitPOAPRequestLink} to view more information`;
-  if (isProd) {
-    await sendOnboardingMessage(msg);
-  } else {
-    await sendDevInternalMessage(msg);
-  }
+
+  await sendOnboardingMessage(msg);
 };
