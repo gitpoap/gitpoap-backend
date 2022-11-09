293881628129c146bf81743aef597733c9737797
Anna Burzillo <burz@gitpoap.io>
2022-11-09 17:20:15 -0500

    Set numRequestedCodes based on the submitted contributors

--- src/lib/gitpoapRequests.ts
+++ src/lib/gitpoapRequests.ts
@@ -1,5 +1,9 @@
 import { context } from '../context';
 import { AdminApprovalStatus } from '@prisma/client';
+import { convertContributorsFromSchema, countContributors } from './gitpoaps';
+import { CUSTOM_GITPOAP_CODE_BUFFER, CUSTOM_GITPOAP_MINIMUM_CODES } from '../constants';
+import { GitPOAPContributorsSchema } from '../schemas/gitpoaps';
+import { z } from 'zod';
 
 export async function deleteGitPOAPRequest(id: number) {
   await context.prisma.gitPOAPRequest.delete({
@@ -15,4 +19,15 @@ export async function updateGitPOAPRequestStatus(
     where: { id: gitPOAPRequestId },
     data: { adminApprovalStatus },
   });
+}
+
+export function chooseNumberOfRequestedCodes(
+  contributors: z.infer<typeof GitPOAPContributorsSchema>,
+): number {
+  const explicitContributors = convertContributorsFromSchema(contributors);
+
+  return Math.max(
+    CUSTOM_GITPOAP_MINIMUM_CODES,
+    CUSTOM_GITPOAP_CODE_BUFFER + countContributors(explicitContributors),
+  );
 }
