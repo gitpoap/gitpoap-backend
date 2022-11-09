da9a11bec3606111306f314b1deb956fe79f75bd
Anna Burzillo <burz@gitpoap.io>
2022-11-09 15:34:30 -0500

    JSON.stringify unknown errors

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -8,7 +8,7 @@ export const errorHandler: ErrorRequestHandler = (err, req, res) => {
     logger.warn(`Returning error status ${err.status} to user: ${err.msg}`);
     res.status(err.status).send(err.msg);
   } else {
-    logger.error(`Caught unknown error: ${err}`);
+    logger.error(`Caught unknown error: ${JSON.stringify(err)}`);
     res.status(500).send(err.message);
   }
 };
