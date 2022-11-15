d0bb67008ddfa1f3fb8b6d3e62fe1c46912adb1f
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-11-15 01:52:53 -0500

    fix: fix email upsert

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -1,5 +1,4 @@
 import { context } from '../context';
-import { createScopedLogger } from '../logging';
 
 export async function upsertEmail(emailAddress: string) {
   return await context.prisma.email.upsert({
@@ -15,36 +14,22 @@ export async function upsertUnverifiedEmail(
   tokenExpiresAt: Date,
   addressId: number,
 ) {
-  const logger = createScopedLogger('upsertUnverifiedEmail');
-  try {
-    return await context.prisma.email.upsert({
-      where: { addressId },
-      update: {},
-      create: {
-        address: {
-          connect: { id: addressId },
-        },
-        emailAddress,
-        activeToken,
-        tokenExpiresAt,
+  return await context.prisma.email.upsert({
+    where: { emailAddress },
+    update: {
+      address: {
+        connect: { id: addressId },
       },
-    });
-  } catch (e) {
-    logger.warn(`Caught error while trying to upsert unverified email ${emailAddress}: ${e}`);
-
-    // Update the record (which we assume to exist) & return it if the upsert fails.
-    const email = await context.prisma.email.update({
-      where: { addressId },
-      data: {
-        address: {
-          connect: { id: addressId },
-        },
-        emailAddress,
-        activeToken,
-        tokenExpiresAt,
+      activeToken,
+      tokenExpiresAt,
+    },
+    create: {
+      address: {
+        connect: { id: addressId },
       },
-    });
-
-    return email;
-  }
+      emailAddress,
+      activeToken,
+      tokenExpiresAt,
+    },
+  });
 }
