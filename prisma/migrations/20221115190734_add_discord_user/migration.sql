f69b50e57a7666c8fcd9e6fa74482753320ffb51
tyler415git <tyler@gitpoap.io>
2022-11-15 11:07:48 -0800

    feat: add discord user schema

---
+++ prisma/migrations/20221115190734_add_discord_user/migration.sql
@@ -0,0 +1,28 @@
+-- AlterTable
+ALTER TABLE "Address" ADD COLUMN     "discordUserId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "AuthToken" ADD COLUMN     "discordUserId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Claim" ADD COLUMN     "discordUserId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "DiscordUser" (
+    "id" SERIAL NOT NULL,
+    "discordHandle" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "discordOAuthToken" TEXT,
+
+    CONSTRAINT "DiscordUser_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_discordUserId_fkey" FOREIGN KEY ("discordUserId") REFERENCES "DiscordUser"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Claim" ADD CONSTRAINT "Claim_discordUserId_fkey" FOREIGN KEY ("discordUserId") REFERENCES "DiscordUser"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AuthToken" ADD CONSTRAINT "AuthToken_discordUserId_fkey" FOREIGN KEY ("discordUserId") REFERENCES "DiscordUser"("id") ON DELETE SET NULL ON UPDATE CASCADE;
