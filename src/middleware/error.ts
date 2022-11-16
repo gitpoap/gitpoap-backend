d9381998d4d6b8f81db7037c121f1ea0014946d2
Anna Burzillo <burz@gitpoap.io>
2022-11-16 12:55:16 -0500

    Remove circular objects in errorHandler stringify

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -2,6 +2,22 @@ import { ErrorRequestHandler } from 'express';
 import { createScopedLogger } from '../logging';
 import { captureException } from '../lib/sentry';
 
+// Based on https://stackoverflow.com/a/69881039/18750275
+function circularReplacer() {
+  const objectsAlreadyVisited = new WeakSet();
+
+  return (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (objectsAlreadyVisited.has(value)) {
+        return '[Circular]';
+      }
+      objectsAlreadyVisited.add(value);
+    }
+
+    return value;
+  };
+}
+
 export const errorHandler: ErrorRequestHandler = (err, req, res) => {
   const logger = createScopedLogger('errorHandler');
 
@@ -9,7 +25,7 @@ export const errorHandler: ErrorRequestHandler = (err, req, res) => {
     logger.warn(`Returning error status ${err.status} to user: ${err.msg}`);
     res.status(err.status).send(err.msg);
   } else {
-    logger.error(`Caught unknown error: ${JSON.stringify(err)}`);
+    logger.error(`Caught unknown error: ${JSON.stringify(err, circularReplacer())}`);
     captureException(err, { service: 'unknownException', function: 'errorHandler' });
     // Don't send users the actual error!
     res.status(500).send('An error occurred on the server');
