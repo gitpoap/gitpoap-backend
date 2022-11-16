1aacd4670451ee3baea8539f7ea63b558badda3b
tyler415git <tyler@gitpoap.io>
2022-11-16 07:45:53 -0800

    feat: add discord auth route

---
+++ src/lib/discordUsers.ts
@@ -0,0 +1,38 @@
+import { context } from '../context';
+import { createScopedLogger } from '../logging';
+
+export async function upsertDiscordUser(
+  discordId: number,
+  discordHandle: string,
+  discordOAuthToken?: string,
+) {
+  const logger = createScopedLogger('upsertDiscordUser');
+
+  logger.info(`Attempting to upsert Discord user ${discordHandle}`);
+
+  return await context.prisma.discordUser.upsert({
+    where: {
+      discordId,
+    },
+    update: {
+      discordHandle,
+      discordOAuthToken,
+    },
+    create: {
+      discordId,
+      discordHandle,
+      discordOAuthToken,
+    },
+  });
+}
+
+export async function removeDiscordUsersDiscordOAuthToken(discordUserId: number) {
+  await context.prisma.discordUser.update({
+    where: {
+      id: discordUserId,
+    },
+    data: {
+      discordOAuthToken: null,
+    },
+  });
+}
