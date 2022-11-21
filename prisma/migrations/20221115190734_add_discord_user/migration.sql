2591097fa20bcd2d4d0bb982349d6afdb54ea4c0
tyler415git <tyler@gitpoap.io>
2022-11-21 05:59:02 -0800

    feat: update discordUser schema

--- prisma/migrations/20221115190734_add_discord_user/migration.sql
+++ prisma/migrations/20221115190734_add_discord_user/migration.sql
@@ -2,9 +2,6 @@
 ALTER TABLE "Address" ADD COLUMN     "discordUserId" INTEGER;
 
 -- AlterTable
-ALTER TABLE "AuthToken" ADD COLUMN     "discordUserId" INTEGER;
-
--- AlterTable
 ALTER TABLE "Claim" ADD COLUMN     "discordUserId" INTEGER;
 
 -- CreateTable
@@ -24,11 +21,14 @@ ALTER TABLE "Address" ADD CONSTRAINT "Address_discordUserId_fkey" FOREIGN KEY ("
 -- AddForeignKey
 ALTER TABLE "Claim" ADD CONSTRAINT "Claim_discordUserId_fkey" FOREIGN KEY ("discordUserId") REFERENCES "DiscordUser"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
--- AddForeignKey
-ALTER TABLE "AuthToken" ADD CONSTRAINT "AuthToken_discordUserId_fkey" FOREIGN KEY ("discordUserId") REFERENCES "DiscordUser"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
 -- AlterTable
 ALTER TABLE "DiscordUser" ADD COLUMN     "discordId" TEXT NOT NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "DiscordUser_discordId_key" ON "DiscordUser"("discordId");
+
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "discordHandle" VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "Profile" ADD COLUMN     "discordHandle" VARCHAR(255);
