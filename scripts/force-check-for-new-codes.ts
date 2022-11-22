d215e6b4f1db5c00ca02d89fa39029ed4796c6f7
Anna Burzillo <burz@gitpoap.io>
2022-11-22 15:01:56 -0500

    Add a script to force check for new codes via POAP API

---
+++ scripts/force-check-for-new-codes.ts
@@ -0,0 +1,58 @@
+import { config } from 'dotenv';
+config();
+
+import 'reflect-metadata';
+import { createScopedLogger, updateLogLevel } from '../src/logging';
+import minimist from 'minimist';
+import { context } from '../src/context';
+import { GitPOAPStatus } from '@prisma/client';
+import { checkGitPOAPForNewCodes } from '../src/lib/codes';
+
+async function forceCheckForNewCodes() {
+  const logger = createScopedLogger('forceCheckForNewCodes');
+
+  // Only run on APPROVED GitPOAPs since the background process checks
+  // the other two states
+  const gitPOAPs = await context.prisma.gitPOAP.findMany({
+    where: { poapApprovalStatus: GitPOAPStatus.APPROVED },
+    select: {
+      id: true,
+      poapApprovalStatus: true,
+      poapEventId: true,
+      poapSecret: true,
+      name: true,
+      type: true,
+      description: true,
+      organization: true,
+      creatorAddress: true,
+      imageUrl: true,
+      creatorEmail: true,
+      gitPOAPRequest: true,
+    },
+  });
+
+  logger.info(`Force-running check for new codes for ${gitPOAPs.length} GitPOAPs`);
+
+  for (const gitPOAP of gitPOAPs) {
+    await checkGitPOAPForNewCodes(gitPOAP);
+  }
+}
+
+const main = async () => {
+  const logger = createScopedLogger('main');
+
+  const argv = minimist(process.argv.slice(2));
+
+  logger.info(`Command line args: ${JSON.stringify(argv)}`);
+
+  updateLogLevel(argv['level']);
+
+  await context.redis.connect();
+  logger.info('Connected to redis');
+
+  await forceCheckForNewCodes();
+
+  await context.redis.disconnect();
+};
+
+void main();
