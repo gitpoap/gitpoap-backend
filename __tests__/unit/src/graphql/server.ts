29357a46776a7caf6626f44731fb4253a5457ea2
Anna Burzillo <burz@gitpoap.io>
2022-12-05 18:25:45 -0500

    Add unit tests for createGQLServer

---
+++ __tests__/unit/src/graphql/server.ts
@@ -0,0 +1,265 @@
+import '../../../../__mocks__/src/logging';
+import { createGQLServer } from '../../../../src/graphql/server';
+import { graphqlHTTP } from 'express-graphql';
+import { verify } from 'jsonwebtoken';
+import { FRONTEND_JWT_SECRET, JWT_SECRET } from '../../../../src/environment';
+import set from 'lodash/set';
+
+jest.mock('../../../../src/logging');
+jest.mock('express-graphql');
+jest.mock('jsonwebtoken');
+jest.mock('lodash/set');
+
+const mockedGraphqlHTTP = jest.mocked(graphqlHTTP, true);
+const mockedGraphqlHTTPHandler = jest.fn();
+const mockedVerify = jest.mocked(verify, true);
+const mockedSet = jest.mocked(set, true);
+
+const genMockedReq = (method: string, path: string) => ({
+  method,
+  path,
+  get: jest.fn(),
+});
+const genMockedRes = () => {
+  const send = jest.fn();
+  const status = jest.fn();
+
+  status.mockReturnValue({
+    send,
+  });
+
+  return {
+    status,
+  };
+};
+
+describe('createGQLServer', () => {
+  beforeEach(() => {
+    mockedGraphqlHTTP.mockReturnValue(mockedGraphqlHTTPHandler);
+  });
+
+  it('Skips checking frontend auth on DEV for graphiql', async () => {
+    const handler = await createGQLServer();
+
+    expect(mockedGraphqlHTTP).toHaveBeenCalledTimes(1);
+
+    const mockedReq = genMockedReq('GET', '/');
+
+    handler(mockedReq as any, {} as any, () => ({}));
+
+    expect(mockedGraphqlHTTPHandler).toHaveBeenCalledTimes(1);
+
+    expect(mockedReq.get).toHaveBeenCalledTimes(0);
+  });
+
+  const genGeneralSetup = async () => {
+    const values = {
+      handler: await createGQLServer(),
+      mockedReq: genMockedReq('POST', '/'),
+      mockedRes: genMockedRes(),
+    };
+
+    expect(mockedGraphqlHTTP).toHaveBeenCalledTimes(1);
+
+    return values;
+  };
+
+  it('Fails when no auth header is provided', async () => {
+    const { handler, mockedReq, mockedRes } = await genGeneralSetup();
+
+    mockedReq.get.mockReturnValue(undefined);
+
+    handler(mockedReq as any, mockedRes as any, () => ({}));
+
+    expect(mockedGraphqlHTTPHandler).toHaveBeenCalledTimes(0);
+
+    expect(mockedReq.get).toHaveBeenCalledTimes(1);
+    expect(mockedReq.get).toHaveBeenCalledWith('authorization');
+
+    expect(mockedRes.status).toHaveBeenCalledTimes(1);
+    expect(mockedRes.status).toHaveBeenCalledWith(401);
+  });
+
+  it('Fails when the authorization is invalid JSON', async () => {
+    const { handler, mockedReq, mockedRes } = await genGeneralSetup();
+
+    mockedReq.get.mockReturnValue('{');
+
+    handler(mockedReq as any, mockedRes as any, () => ({}));
+
+    expect(mockedReq.get).toHaveBeenCalledTimes(1);
+    expect(mockedReq.get).toHaveBeenCalledWith('authorization');
+
+    expect(mockedRes.status).toHaveBeenCalledTimes(1);
+    expect(mockedRes.status).toHaveBeenCalledWith(401);
+
+    expect(mockedVerify).toHaveBeenCalledTimes(0);
+
+    expect(mockedGraphqlHTTPHandler).toHaveBeenCalledTimes(0);
+  });
+
+  it('Fails when the authorization is missing fields', async () => {
+    const { handler, mockedReq, mockedRes } = await genGeneralSetup();
+
+    const frontendToken = 'foobar';
+    mockedReq.get.mockReturnValue(
+      JSON.stringify({
+        frontend: frontendToken,
+        user: null,
+      }),
+    );
+    mockedVerify.mockImplementationOnce(() => {
+      throw new Error('error');
+    });
+
+    handler(mockedReq as any, mockedRes as any, () => ({}));
+
+    expect(mockedReq.get).toHaveBeenCalledTimes(1);
+    expect(mockedReq.get).toHaveBeenCalledWith('authorization');
+
+    expect(mockedRes.status).toHaveBeenCalledTimes(1);
+    expect(mockedRes.status).toHaveBeenCalledWith(401);
+
+    expect(mockedVerify).toHaveBeenCalledTimes(1);
+    expect(mockedVerify).toHaveBeenCalledWith(frontendToken, FRONTEND_JWT_SECRET);
+
+    expect(mockedGraphqlHTTPHandler).toHaveBeenCalledTimes(0);
+  });
+
+  it('Succeeds when frontend token is valid without user token specified', async () => {
+    const { handler, mockedReq, mockedRes } = await genGeneralSetup();
+
+    const frontendToken = 'foobar';
+    mockedReq.get.mockReturnValue(
+      JSON.stringify({
+        frontend: frontendToken,
+        user: null,
+      }),
+    );
+
+    handler(mockedReq as any, mockedRes as any, () => ({}));
+
+    expect(mockedReq.get).toHaveBeenCalledTimes(1);
+    expect(mockedReq.get).toHaveBeenCalledWith('authorization');
+
+    expect(mockedRes.status).toHaveBeenCalledTimes(0);
+
+    expect(mockedVerify).toHaveBeenCalledTimes(1);
+    expect(mockedVerify).toHaveBeenCalledWith(frontendToken, FRONTEND_JWT_SECRET);
+
+    expect(mockedSet).toHaveBeenCalledTimes(1);
+    expect(mockedSet).toHaveBeenCalledWith(mockedReq, 'user', null);
+
+    expect(mockedGraphqlHTTPHandler).toHaveBeenCalledTimes(1);
+  });
+
+  it('Succeeds when frontend token is valid but not user token is invalid', async () => {
+    const { handler, mockedReq, mockedRes } = await genGeneralSetup();
+
+    const frontendToken = 'foobar';
+    const userToken = 'yay';
+    mockedReq.get.mockReturnValue(
+      JSON.stringify({
+        frontend: frontendToken,
+        user: userToken,
+      }),
+    );
+    mockedVerify
+      .mockImplementationOnce(() => true)
+      .mockImplementationOnce(() => {
+        throw new Error('error');
+      });
+
+    handler(mockedReq as any, mockedRes as any, () => ({}));
+
+    expect(mockedReq.get).toHaveBeenCalledTimes(1);
+    expect(mockedReq.get).toHaveBeenCalledWith('authorization');
+
+    expect(mockedRes.status).toHaveBeenCalledTimes(0);
+
+    expect(mockedVerify).toHaveBeenCalledTimes(2);
+    expect(mockedVerify).toHaveBeenNthCalledWith(1, frontendToken, FRONTEND_JWT_SECRET);
+    expect(mockedVerify).toHaveBeenNthCalledWith(2, userToken, JWT_SECRET);
+
+    expect(mockedSet).toHaveBeenCalledTimes(1);
+    expect(mockedSet).toHaveBeenCalledWith(mockedReq, 'user', null);
+
+    expect(mockedGraphqlHTTPHandler).toHaveBeenCalledTimes(1);
+  });
+
+  it('Succeeds when frontend token is valid but not user token is malformed', async () => {
+    const { handler, mockedReq, mockedRes } = await genGeneralSetup();
+
+    const frontendToken = 'foobar';
+    const userToken = 'yay';
+    mockedReq.get.mockReturnValue(
+      JSON.stringify({
+        frontend: frontendToken,
+        user: userToken,
+      }),
+    );
+    mockedVerify
+      .mockImplementationOnce(() => true)
+      .mockImplementationOnce(() => ({
+        foo: 'bar',
+      }));
+
+    handler(mockedReq as any, mockedRes as any, () => ({}));
+
+    expect(mockedReq.get).toHaveBeenCalledTimes(1);
+    expect(mockedReq.get).toHaveBeenCalledWith('authorization');
+
+    expect(mockedRes.status).toHaveBeenCalledTimes(0);
+
+    expect(mockedVerify).toHaveBeenCalledTimes(2);
+    expect(mockedVerify).toHaveBeenNthCalledWith(1, frontendToken, FRONTEND_JWT_SECRET);
+    expect(mockedVerify).toHaveBeenNthCalledWith(2, userToken, JWT_SECRET);
+
+    expect(mockedSet).toHaveBeenCalledTimes(1);
+    expect(mockedSet).toHaveBeenCalledWith(mockedReq, 'user', null);
+
+    expect(mockedGraphqlHTTPHandler).toHaveBeenCalledTimes(1);
+  });
+
+  it('Succeeds when frontend token is valid and user token is valid', async () => {
+    const { handler, mockedReq, mockedRes } = await genGeneralSetup();
+
+    const frontendToken = 'foobar';
+    const userToken = 'yay';
+    mockedReq.get.mockReturnValue(
+      JSON.stringify({
+        frontend: frontendToken,
+        user: userToken,
+      }),
+    );
+    const userPayload = {
+      authTokenId: 2,
+      addressId: 342,
+      address: '0xfoo',
+      ensName: null,
+      ensAvatarImageUrl: null,
+      githubId: null,
+      githubHandle: null,
+      discordId: null,
+      discordHandle: null,
+      emailId: null,
+    };
+    mockedVerify.mockImplementationOnce(() => true).mockImplementationOnce(() => userPayload);
+
+    handler(mockedReq as any, mockedRes as any, () => ({}));
+
+    expect(mockedReq.get).toHaveBeenCalledTimes(1);
+    expect(mockedReq.get).toHaveBeenCalledWith('authorization');
+
+    expect(mockedRes.status).toHaveBeenCalledTimes(0);
+
+    expect(mockedVerify).toHaveBeenCalledTimes(2);
+    expect(mockedVerify).toHaveBeenNthCalledWith(1, frontendToken, FRONTEND_JWT_SECRET);
+    expect(mockedVerify).toHaveBeenNthCalledWith(2, userToken, JWT_SECRET);
+
+    expect(mockedSet).toHaveBeenCalledTimes(1);
+    expect(mockedSet).toHaveBeenCalledWith(mockedReq, 'user', userPayload);
+
+    expect(mockedGraphqlHTTPHandler).toHaveBeenCalledTimes(1);
+  });
+});
