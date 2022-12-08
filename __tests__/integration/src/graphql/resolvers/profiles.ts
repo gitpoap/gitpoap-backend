c63b4e5bbd476eeaeb012f56531e064164562551
Anna Burzillo <burz@gitpoap.io>
2022-12-08 15:54:41 -0500

    Increase wait time for all tests

--- __tests__/integration/src/graphql/resolvers/profiles.ts
+++ __tests__/integration/src/graphql/resolvers/profiles.ts
@@ -1,6 +1,5 @@
 import { GraphQLClient, gql } from 'graphql-request';
 import { ADDRESSES } from '../../../../../prisma/constants';
-import { MILLISECONDS_PER_SECOND } from '../../../../../src/constants';
 import { context } from '../../../../../src/context';
 
 describe('CustomProfileResolver', () => {
@@ -26,120 +25,104 @@ describe('CustomProfileResolver', () => {
     expect(data.lastMonthContributors).toEqual(1);
   });
 
-  it(
-    'profileData',
-    async () => {
-      const data = await client.request(gql`
-      {
-        profileData(address: "${ADDRESSES.burz}") {
-          ensName
-          githubHandle
-        }
+  it('profileData', async () => {
+    const data = await client.request(gql`
+    {
+      profileData(address: "${ADDRESSES.burz}") {
+        ensName
+        githubHandle
       }
-    `);
+    }
+  `);
 
-      expect(data.profileData).not.toEqual(null);
-      expect(data.profileData.ensName).toEqual('burz.eth');
-      expect(data.profileData.githubHandle).toEqual('burz');
-    },
-    10 * MILLISECONDS_PER_SECOND,
-  );
+    expect(data.profileData).not.toEqual(null);
+    expect(data.profileData.ensName).toEqual('burz.eth');
+    expect(data.profileData.githubHandle).toEqual('burz');
+  });
 
-  it(
-    'profileData - nullable',
-    async () => {
-      const address = ADDRESSES.jay.substring(0, 5) + 'c' + ADDRESSES.jay.substring(6);
+  it('profileData - nullable', async () => {
+    const address = ADDRESSES.jay.substring(0, 5) + 'c' + ADDRESSES.jay.substring(6);
 
-      // Ensure the record doesn't already exist
-      const startingRecord = await context.prisma.profile.findFirst({
-        where: {
-          address: {
-            ethAddress: address,
-          },
+    // Ensure the record doesn't already exist
+    const startingRecord = await context.prisma.profile.findFirst({
+      where: {
+        address: {
+          ethAddress: address,
         },
-      });
+      },
+    });
 
-      expect(startingRecord).toEqual(null);
+    expect(startingRecord).toEqual(null);
 
-      const data = await client.request(gql`
-      {
-        profileData(address: "${address}") {
-          ensName
-          address
-          name
-          githubHandle
-          isVisibleOnLeaderboard
-        }
+    const data = await client.request(gql`
+    {
+      profileData(address: "${address}") {
+        ensName
+        address
+        name
+        githubHandle
+        isVisibleOnLeaderboard
       }
-    `);
+    }
+  `);
 
-      expect(data.profileData).not.toEqual(null);
-      expect(data.profileData.ensName).toEqual(null);
-      expect(data.profileData.address).toEqual(address);
-      expect(data.profileData.name).toEqual(null);
-      expect(data.profileData.githubHandle).toEqual(null);
-      expect(data.profileData.isVisibleOnLeaderboard).toEqual(true);
+    expect(data.profileData).not.toEqual(null);
+    expect(data.profileData.ensName).toEqual(null);
+    expect(data.profileData.address).toEqual(address);
+    expect(data.profileData.name).toEqual(null);
+    expect(data.profileData.githubHandle).toEqual(null);
+    expect(data.profileData.isVisibleOnLeaderboard).toEqual(true);
 
-      // Ensure that the record has been upserted
-      const endingRecord = await context.prisma.profile.findFirst({
-        where: {
-          address: {
-            ethAddress: address,
-          },
+    // Ensure that the record has been upserted
+    const endingRecord = await context.prisma.profile.findFirst({
+      where: {
+        address: {
+          ethAddress: address,
         },
-      });
+      },
+    });
 
-      expect(endingRecord).not.toEqual(null);
+    expect(endingRecord).not.toEqual(null);
 
-      // Delete the record
-      await context.prisma.profile.deleteMany({
-        where: {
-          address: {
-            ethAddress: address,
-          },
+    // Delete the record
+    await context.prisma.profile.deleteMany({
+      where: {
+        address: {
+          ethAddress: address,
         },
-      });
-    },
-    10 * MILLISECONDS_PER_SECOND,
-  );
+      },
+    });
+  });
 
-  it(
-    'profileData - new GitHub Connection',
-    async () => {
-      const data = await client.request(gql`
-      {
-        profileData(address: "${ADDRESSES.aldo}") {
-          address
-          githubHandle
-        }
+  it('profileData - new GitHub Connection', async () => {
+    const data = await client.request(gql`
+    {
+      profileData(address: "${ADDRESSES.aldo}") {
+        address
+        githubHandle
       }
-    `);
+    }
+  `);
 
-      expect(data.profileData).not.toEqual(null);
-      expect(data.profileData.address).toEqual(ADDRESSES.aldo);
-      expect(data.profileData.githubHandle).toEqual('aldolamb');
-    },
-    10 * MILLISECONDS_PER_SECOND,
-  );
+    expect(data.profileData).not.toEqual(null);
+    expect(data.profileData.address).toEqual(ADDRESSES.aldo);
+    expect(data.profileData.githubHandle).toEqual('aldolamb');
+  });
 
-  it(
-    'profileData - old GitHub Connection',
-    async () => {
-      const data = await client.request(gql`
-      {
-        profileData(address: "${ADDRESSES.random}") {
-          address
-          githubHandle
-        }
+  it('profileData - old GitHub Connection', async () => {
+    const data = await client.request(gql`
+    {
+      profileData(address: "${ADDRESSES.random}") {
+        address
+        githubHandle
       }
-    `);
+    }
+  `);
 
-      expect(data.profileData).not.toEqual(null);
-      expect(data.profileData.address).toEqual(ADDRESSES.random);
-      expect(data.profileData.githubHandle).toEqual('randomHandle');
-    },
-    10 * MILLISECONDS_PER_SECOND,
-  );
+    expect(data.profileData).not.toEqual(null);
+    expect(data.profileData.address).toEqual(ADDRESSES.random);
+    expect(data.profileData.githubHandle).toEqual('randomHandle');
+  });
 
   it('mostHonoredContributors', async () => {
     const data = await client.request(gql`
