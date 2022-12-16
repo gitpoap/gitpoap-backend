b99017c3e4423cab3fb806426f482d0dd70dfbbf
Anna Burzillo <burz@gitpoap.io>
2022-12-16 01:06:04 -0500

    Remove requirement for frontend tokens for GQL

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,6 @@ import env from 'env-var';
 export const APP_NAME = env.get('APP_NAME').required().asString();
 
 export const JWT_SECRET = env.get('JWT_SECRET').required().asString();
-export const FRONTEND_JWT_SECRET = env.get('FRONTEND_JWT_SECRET').required().asString();
 
 export const NODE_ENV = env.get('NODE_ENV').required().asString();
 
