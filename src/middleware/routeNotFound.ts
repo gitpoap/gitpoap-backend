3cdc34b79412dd66226ab866c7dd6f949bfbc2fa
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-12-23 12:21:06 -0500

    chore: switch logger from error to warn for route not found

--- src/middleware/routeNotFound.ts
+++ src/middleware/routeNotFound.ts
@@ -3,7 +3,7 @@ import { getRequestLogger } from './loggingAndTiming';
 
 export const routeNotFoundHandler: RequestHandler = (req, res) => {
   // Note that the request logger already prints out the route
-  getRequestLogger(req).error(`Unknown route called!`);
+  getRequestLogger(req).warn('Unknown route called!');
 
   return res.status(404).send({
     msg: `Route ${req.method} ${req.path} not found!`,
