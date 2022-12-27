e6e4350e5ee2fd99dd8ee6985137adc34d4cd333
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-12-26 20:27:25 -0500

    test: add sample unit tests for GQL resolvesr

--- __mocks__/src/graphql/server.ts
+++ __mocks__/src/graphql/server.ts
@@ -2,19 +2,24 @@ import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '../../../src/environment';
 import { GraphQLClient } from 'graphql-request';
 
-function genGQLAccessToken(userPayload?: Record<string, any>) {
-  let user: string | null = null;
+export function genGQLAccessToken(userPayload?: Record<string, any>) {
+  let token: string | null = null;
   if (userPayload !== undefined) {
-    user = sign(userPayload, JWT_SECRET);
+    token = sign(userPayload, JWT_SECRET);
   }
 
-  return `Bearer ${user}`;
+  return token;
+}
+
+export function genAuthHeader(userPayload?: Record<string, any>) {
+  const token = genGQLAccessToken(userPayload);
+  return `Bearer ${token}`;
 }
 
 export function getGraphQLClient(userPayload?: Record<string, any>) {
   return new GraphQLClient('http://server:3001/graphql', {
     headers: {
-      authorization: genGQLAccessToken(userPayload),
+      authorization: genAuthHeader(userPayload),
     },
   });
 }
