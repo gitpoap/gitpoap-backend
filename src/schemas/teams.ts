1a7be5372c11d790487f79c1a9cfb11ea2e62d44
tyler415git <tyler@gitpoap.io>
2023-01-06 11:26:21 -0800

    feat: update team description validation

--- src/schemas/teams.ts
+++ src/schemas/teams.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const CreateTeamSchema = z.object({
   name: z.string().nonempty(),
-  description: z.string().nonempty().optional(),
+  description: z.string().optional(),
   // Only can be set by GitPOAP staff members
   adminAddressId: z.number().optional(),
 });
