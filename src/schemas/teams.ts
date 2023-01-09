e88d76d71a42fdc75bf9f912ea6caf3e6c17cd46
Anna Burzillo <burz@gitpoap.io>
2023-01-09 13:14:20 -0500

    Allow staff to set a separate admin address id for a team

--- src/schemas/teams.ts
+++ src/schemas/teams.ts
@@ -3,4 +3,6 @@ import { z } from 'zod';
 export const CreateTeamSchema = z.object({
   name: z.string(),
   description: z.string().nonempty().optional(),
+  // Only can be set by GitPOAP staff members
+  adminAddressId: z.number().optional(),
 });
