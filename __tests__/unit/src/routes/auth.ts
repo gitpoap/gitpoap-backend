3bee0c2a0bbacfd6519b57ea65daeb5047856051
Anna Burzillo <burz@gitpoap.io>
2023-01-24 14:27:26 -0500

    Fix existing auth test compilation

--- __tests__/unit/src/routes/auth.ts
+++ __tests__/unit/src/routes/auth.ts
@@ -1,71 +1,61 @@
 import '../../../../__mocks__/src/logging';
 import request from 'supertest';
 import { setupApp } from '../../../../__mocks__/src/app';
-import { isAuthSignatureDataValid } from '../../../../src/lib/signatures';
-import { upsertAddress } from '../../../../src/lib/addresses';
 import { generateNewAuthTokens } from '../../../../src/lib/authTokens';
+import { verifyPrivyToken } from '../../../../src/lib/privy';
 
 jest.mock('../../../../src/logging');
-jest.mock('../../../../src/lib/signatures');
-jest.mock('../../../../src/lib/ens');
-jest.mock('../../../../src/lib/addresses');
 jest.mock('../../../../src/lib/authTokens');
+jest.mock('../../../../src/lib/privy');
 
-const mockedIsAuthSignatureDataValid = jest.mocked(isAuthSignatureDataValid, true);
-const mockedUpsertAddress = jest.mocked(upsertAddress, true);
 const mockedGenerateNewAuthTokens = jest.mocked(generateNewAuthTokens, true);
+const mockedVerifyPrivyToken = jest.mocked(verifyPrivyToken, true);
 
-const addressId = 22;
-const address = '0xburzyBurz';
-const signatureData = {
-  signature: 'John Hancock',
-  message: 'The pen is mightier than the sword',
-  createdAt: 3423423425,
-};
-
+const privyToken = 'im-a-privy-cool-person';
+const fakePrivyUserData = { fake: 'data' };
 const authTokens = { accessToken: 'foo' };
 
 describe('POST /auth', () => {
   it('Fails with invalid body', async () => {
     const result = await request(await setupApp())
       .post('/auth')
-      .send({ address });
+      .send({ body: 'missing' });
 
     expect(result.statusCode).toEqual(400);
+
+    expect(mockedVerifyPrivyToken).toHaveBeenCalledTimes(0);
   });
 
-  it('Fails with invalid signature', async () => {
-    mockedIsAuthSignatureDataValid.mockReturnValue(false);
+  it("Fails when the Privy token can't be validated", async () => {
+    mockedVerifyPrivyToken.mockResolvedValue(null);
 
     const result = await request(await setupApp())
       .post('/auth')
-      .send({ address, signatureData });
+      .send({ privyToken });
 
     expect(result.statusCode).toEqual(401);
 
-    expect(mockedIsAuthSignatureDataValid).toHaveBeenCalledTimes(1);
-    expect(mockedIsAuthSignatureDataValid).toHaveBeenCalledWith(address, signatureData);
+    expect(mockedVerifyPrivyToken).toHaveBeenCalledTimes(1);
+    expect(mockedVerifyPrivyToken).toHaveBeenCalledWith(privyToken);
+
+    expect(mockedGenerateNewAuthTokens).toHaveBeenCalledTimes(0);
   });
 
-  it('Succeeds when signature is valid', async () => {
-    mockedIsAuthSignatureDataValid.mockReturnValue(true);
-    mockedUpsertAddress.mockResolvedValue({ id: addressId } as any);
+  it('Returns new auth tokens when privy validation is successful', async () => {
+    mockedVerifyPrivyToken.mockResolvedValue(fakePrivyUserData as any);
     mockedGenerateNewAuthTokens.mockResolvedValue(authTokens);
 
     const result = await request(await setupApp())
       .post('/auth')
-      .send({ address, signatureData });
+      .send({ privyToken });
 
     expect(result.statusCode).toEqual(200);
     expect(result.body).toEqual(authTokens);
 
-    expect(mockedIsAuthSignatureDataValid).toHaveBeenCalledTimes(1);
-    expect(mockedIsAuthSignatureDataValid).toHaveBeenCalledWith(address, signatureData);
-
-    expect(mockedUpsertAddress).toHaveBeenCalledTimes(1);
-    expect(mockedUpsertAddress).toHaveBeenCalledWith(address);
+    expect(mockedVerifyPrivyToken).toHaveBeenCalledTimes(1);
+    expect(mockedVerifyPrivyToken).toHaveBeenCalledWith(privyToken);
 
     expect(mockedGenerateNewAuthTokens).toHaveBeenCalledTimes(1);
-    expect(mockedGenerateNewAuthTokens).toHaveBeenCalledWith(addressId);
+    expect(mockedGenerateNewAuthTokens).toHaveBeenCalledWith(fakePrivyUserData);
   });
 });
