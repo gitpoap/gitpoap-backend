d35ff8b25a5212691ff6f5da85babfc2c56cd491
Anna Burzillo <burz@gitpoap.io>
2023-01-25 13:21:15 -0500

    Fix integration tests

--- __mocks__/src/graphql/server.ts
+++ __mocks__/src/graphql/server.ts
@@ -1,25 +1,82 @@
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '../../../src/environment';
 import { GraphQLClient } from 'graphql-request';
+import { context } from '../../../src/context';
+import {
+  AccessTokenPayload,
+  AddressPayload,
+  DiscordPayload,
+  EmailPayload,
+  GithubPayload,
+} from '../../../src/types/authTokens';
 
-export function genGQLAccessToken(userPayload?: Record<string, any>) {
-  let token: string | null = null;
+export function genGQLAccessToken(userPayload?: AccessTokenPayload) {
   if (userPayload !== undefined) {
-    token = sign(userPayload, JWT_SECRET);
+    return sign(userPayload, JWT_SECRET);
   }
 
-  return token;
+  return 'null';
 }
 
-export function genAuthHeader(userPayload?: Record<string, any>) {
-  const token = genGQLAccessToken(userPayload);
-  return `Bearer ${token}`;
-}
-
-export function getGraphQLClient(userPayload?: Record<string, any>) {
+export function getGraphQLClient(userPayload?: AccessTokenPayload) {
   return new GraphQLClient('http://server:3001/graphql', {
     headers: {
-      authorization: genAuthHeader(userPayload),
+      authorization: `Bearer ${genGQLAccessToken(userPayload)}`,
     },
+  });
+}
+
+type AuthSpec = {
+  ethAddress?: string;
+  githubHandle?: string;
+  emailAddress?: string;
+  discordHandle?: string;
+};
+
+export async function getGraphQLClientWithAuth({
+  ethAddress,
+  githubHandle,
+  emailAddress,
+  discordHandle,
+}: AuthSpec) {
+  let address: AddressPayload | null = null;
+  if (ethAddress !== undefined) {
+    address = await context.prisma.address.findUnique({
+      where: { ethAddress },
+    });
+    expect(address).not.toEqual(null);
+  }
+
+  let github: GithubPayload | null = null;
+  if (githubHandle !== undefined) {
+    github = await context.prisma.githubUser.findFirst({
+      where: { githubHandle },
+    });
+    expect(github).not.toEqual(null);
+  }
+
+  let email: EmailPayload | null = null;
+  if (emailAddress !== undefined) {
+    email = await context.prisma.email.findUnique({
+      where: { emailAddress },
+    });
+    expect(email).not.toEqual(null);
+  }
+
+  let discord: DiscordPayload | null = null;
+  if (discordHandle !== undefined) {
+    discord = await context.prisma.discordUser.findFirst({
+      where: { discordHandle },
+    });
+    expect(discord).not.toEqual(null);
+  }
+
+  return getGraphQLClient({
+    privyUserId: 'foobar',
+    address,
+    github,
+    email,
+    discord,
+    memberships: [],
   });
 }
