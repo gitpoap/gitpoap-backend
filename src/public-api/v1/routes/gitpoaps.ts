1bb4ebec26154bd253f08c530e6cebfaeb9f77c5
Anna Burzillo <burz@gitpoap.io>
2023-04-14 12:56:21 -0400

    Complete initial implementation

--- src/public-api/v1/routes/gitpoaps.ts
+++ src/public-api/v1/routes/gitpoaps.ts
@@ -3,6 +3,8 @@ import { context } from '../../../context';
 import { ClaimStatus } from '@generated/type-graphql';
 import { mapGitPOAPsToGitPOAPResults } from '../helpers';
 import { getRequestLogger } from '../../../middleware/loggingAndTiming';
+import { z } from 'zod';
+import { mapClaimsToGitPOAPResults } from '../helpers';
 
 export const gitpoapsRouter = Router();
 
@@ -38,7 +40,7 @@ gitpoapsRouter.get('/:gitpoapId/addresses', async function (req, res) {
 
   const mappedAddresses = claims.map(claim => claim.mintedAddress?.ethAddress);
 
-  logger.info(
+  logger.debug(
     `Completed request to get all addresses that possess GitPOAP id ${req.params.gitpoapId}`,
   );
 
@@ -122,4 +124,123 @@ gitpoapsRouter.get('/events', async (req, res) => {
   logger.debug('Completed request for all GitPOAP events');
 
   return res.status(200).send({ gitPoapEvents: results });
+});
+
+const BackfillSchema = z.object({
+  page: z.number().positive(),
+  perPage: z.number().optional(),
+  fromId: z.number().positive().optional(),
+});
+
+const MAX_GITPOAPS_PER_PAGE = 100;
+
+gitpoapsRouter.get('/backfill', async function (req, res) {
+  const logger = getRequestLogger(req);
+
+  const schemaResult = BackfillSchema.safeParse(req.body);
+  if (!schemaResult.success) {
+    logger.warn(
+      `Missing/invalid body fields in request: ${JSON.stringify(schemaResult.error.issues)}`,
+    );
+    return res.status(400).send({ issues: schemaResult.error.issues });
+  }
+
+  const page = schemaResult.data.page;
+  let perPage = MAX_GITPOAPS_PER_PAGE;
+  if (schemaResult.data.perPage !== undefined) {
+    if (schemaResult.data.perPage > MAX_GITPOAPS_PER_PAGE) {
+      logger.warn(
+        `User requested too many GitPOAPs per page: ${schemaResult.data.perPage} > ${MAX_GITPOAPS_PER_PAGE}`,
+      );
+      return res.status(400).send({
+        msg: `You can only request at most ${MAX_GITPOAPS_PER_PAGE} GitPOAPs per page`,
+      });
+    }
+    perPage = schemaResult.data.perPage;
+  }
+
+  logger.info(
+    `Request for GitPOAPs with IDs greater than ${
+      schemaResult.data.fromId ?? 0
+    } (page: ${page}, per page: ${perPage}`,
+  );
+
+  let id;
+  if (schemaResult.data.fromId !== undefined) {
+    id = { gt: schemaResult.data.fromId };
+  }
+
+  const claims = await context.prisma.claim.findMany({
+    where: {
+      id,
+      status: ClaimStatus.CLAIMED,
+    },
+    orderBy: { id: 'asc' },
+    skip: page - 1,
+    take: perPage,
+    select: {
+      id: true,
+      createdAt: true,
+      mintedAt: true,
+      poapTokenId: true,
+      needsRevalidation: true,
+      gitPOAP: {
+        select: {
+          id: true,
+          year: true,
+          poapEventId: true,
+          poapApprovalStatus: true,
+          type: true,
+          project: {
+            select: {
+              repos: {
+                select: {
+                  name: true,
+                  organization: {
+                    select: { name: true },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      pullRequestEarned: {
+        select: {
+          githubMergedAt: true,
+        },
+      },
+      mentionEarned: {
+        select: {
+          pullRequest: {
+            select: {
+              githubCreatedAt: true,
+            },
+          },
+          issue: {
+            select: {
+              githubCreatedAt: true,
+            },
+          },
+          githubMentionedAt: true,
+        },
+      },
+    },
+  });
+
+  logger.debug(
+    `Completed request for GitPOAPs with IDs greater than ${
+      schemaResult.data.fromId ?? 0
+    } (page: ${page}, per page: ${perPage}`,
+  );
+
+  const results = await mapClaimsToGitPOAPResults(claims);
+
+  if (results === null) {
+    const msg = 'Failed to query POAP data for claims';
+    logger.error(msg);
+    return res.status(500).send({ msg });
+  }
+
+  return res.status(200).send(results);
 });
