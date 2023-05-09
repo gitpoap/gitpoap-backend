ec57dadd007961aa7dca7a58edb5ec1753fe92fb
Anna Burzillo <burz@gitpoap.io>
2023-05-09 17:04:46 -0400

    Remove onboarding routes

--- src/external/postmark.ts
+++ src/external/postmark.ts
@@ -1,9 +1,5 @@
 import { Errors, ServerClient } from 'postmark';
 import { POSTMARK_SERVER_TOKEN } from '../environment';
-import { IntakeFormReposSchema } from '../schemas/onboarding';
-import { z } from 'zod';
-import { formatRepos } from '../routes/onboarding/utils';
-import { IntakeForm } from '../routes/onboarding/types';
 import { GitPOAPRequestEmailForm, GitPOAPRequestEmailAlias } from '../types/gitpoaps';
 import { generateGitPOAPRequestLink, generateGitPOAPLink } from '../routes/gitpoaps/utils';
 import { createScopedLogger } from '../logging';
@@ -64,31 +60,6 @@ const sendEmailWithTemplateHandler = async <T extends Record<string, string | nu
   }
 };
 
-type SendTextEmailArgs = {
-  to: string;
-  from: string;
-  subject: string;
-  textBody: string;
-};
-
-const sendTextEmailHandler = async ({ to, from, subject, textBody }: SendTextEmailArgs) => {
-  const logger = createScopedLogger('sendTextEmailHandler');
-  try {
-    logger.info(`Sending email to: ${to}, from: ${from}, ${subject}`);
-    const response = await postmarkClient.sendEmail({
-      From: from,
-      To: to,
-      Subject: subject,
-      TextBody: textBody,
-    });
-
-    return response;
-  } catch (e) {
-    logger.error(`Failed to send text email - ${e}`);
-    return null;
-  }
-};
-
 /* -- Specific Email Dispatch Functions -- */
 export const sendVerificationEmail = async (email: string, activeToken: string) =>
   await sendEmailWithTemplateHandler({
@@ -100,59 +71,6 @@ export const sendVerificationEmail = async (email: string, activeToken: string) 
       token: activeToken,
     },
   });
-
-export const sendConfirmationEmail = async (
-  githubHandle: string,
-  formData: IntakeForm,
-  queueNumber: number | undefined,
-) =>
-  await sendEmailWithTemplateHandler({
-    to: formData.email,
-    from: TEAM_EMAIL,
-    alias: 'welcome-1',
-    templateModel: {
-      ...baseTemplate,
-      queue_number: queueNumber ?? '',
-      name: formData.name,
-      email: formData.email,
-      githubHandle,
-      shouldGitPOAPDesign: formData.shouldGitPOAPDesign === 'true' ? 'GitPOAP' : 'You',
-      isOneGitPOAPPerRepo: formData.isOneGitPOAPPerRepo === 'true' ? 'One Per Repo' : 'One For All',
-      notes: formData.notes,
-      repos: formatRepos(JSON.parse(formData.repos)),
-    },
-  });
-
-export const sendInternalConfirmationEmail = async (
-  githubHandle: string,
-  formData: IntakeForm,
-  queueNumber: number | undefined,
-  urls: string[],
-) => {
-  await sendTextEmailHandler({
-    to: TEAM_EMAIL,
-    from: TEAM_EMAIL,
-    subject: `New intake form submission from ${githubHandle} / ${formData.email} `,
-    textBody: `
-    New intake form submission from ${githubHandle} / ${formData.email}
-    Queue number: ${queueNumber ?? ''}
-    Name: ${formData.name}
-    Email: ${formData.email}
-    Notes: ${formData.notes}
-    Github Handle: ${githubHandle}
-    Should GitPOAP Design: ${formData.shouldGitPOAPDesign}
-    Is One GitPOAP Per Repo: ${formData.isOneGitPOAPPerRepo}
-    \n
-    Repos:
-    ${JSON.parse(formData.repos).map(
-      (repo: z.infer<typeof IntakeFormReposSchema>[number]) => repo.full_name,
-    )}
-    \n
-    Images:
-    ${urls.join('\n')}
-    `,
-  });
-};
 
 export const sendGitPOAPRequestConfirmationEmail = async (formData: GitPOAPRequestEmailForm) =>
   await sendGitPOAPRequestEmail(
